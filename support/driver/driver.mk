8285946a5fbda700018012f1c2450522a598fe01
seb <sebastien.caux@univ-bpclermont.fr>
2016-04-08 09:58:43 +0200

    driver.mk added BADDRIVERS

--- support/driver/driver.mk
+++ support/driver/driver.mk
@@ -4,10 +4,17 @@
 define checkfile
 ifneq "$(wildcard $(RTPROG)/support/driver/$(1)/$(1).mk)" ""
 DRIVER_FILES += $(RTPROG)/support/driver/$(1)/$(1).mk
+IDRIVERS += $(1)
+else
+BADDRIVERS += $(1)
 endif
 endef
 
+define includedrivers
 $(foreach DRIVER, $(DRIVERS), $(eval $(call checkfile,$(DRIVER))))
+endef
+
+$(eval $(includedrivers))
 
 -include $(DRIVER_FILES)
 
