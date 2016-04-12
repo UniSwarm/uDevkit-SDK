64f6300ff6ba2ef2c7140e1075bbe4c32bca0b77
sebcaux <sebastien.caux@robotips.fr>
2016-04-12 21:36:35 +0200

    Includes for architecture

---
+++ include/archi.h
@@ -0,0 +1,8 @@
+
+#if defined(ARCHI_dspic33ep)
+ #include "../support/archi/dspic33ep/dspic33ep.h"
+#elif defined(ARCHI_dspic33fj)
+ #include "../support/archi/dspic33fj/dspic33fj.h"
+#else
+ #error Unsuported ARCHI
+#endif
