80a4191eb61ca1b0a32342a3ae918516ec4aebc9
sebcaux <sebastien.caux@robotips.fr>
2016-04-13 00:51:10 +0200

    Complete board.h

--- support/board/rtboard/rtboard.h
+++ support/board/rtboard/rtboard.h
@@ -11,6 +11,28 @@
 #ifndef RTBOARD_H
 #define RTBOARD_H
 
+#include <xc.h>
+
 #define BOARD_NAME "rtboard"
+#define SYSCLOCK_XTAL 8000000
+
+// pins define
+#define LED LATDbits.LATD11
+
+#define M1A LATBbits.LATB14
+#define M1B LATBbits.LATB15
+#define M1en LATDbits.LATD0
+
+#define M2A LATBbits.LATB12
+#define M2B LATBbits.LATB13
+#define M2en LATFbits.LATF3
+
+#define M3A LATBbits.LATB8
+#define M3B LATBbits.LATB9
+#define M3en LATFbits.LATF4
+
+#define M4A LATBbits.LATB10
+#define M4B LATBbits.LATB11
+#define M4en LATFbits.LATF5
 
 #endif // RTBOARD_H
