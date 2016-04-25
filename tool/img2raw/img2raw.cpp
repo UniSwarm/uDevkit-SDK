8fc29ef3b6ff0b76cab0b95392fb958b66db9f9a
sebcaux <sebastien.caux@robotips.fr>
2016-04-25 19:31:40 +0200

    [img2ram] Added tool to generate raw image from image file for lcd support

---
+++ tool/img2raw/img2raw.cpp
@@ -0,0 +1,87 @@
+/**
+ * @file img2raw.c
+ * @author Sebastien CAUX (sebcaux)
+ * @copyright Robotips 2016
+ *
+ * @date April 25, 2016, 18:39 PM 
+ * 
+ * @brief Tool to transform image in const C raw data (xc16 format)
+ */
+
+#include <QCoreApplication>
+
+#include <QFile>
+#include <QDir>
+#include <QTextStream>
+#include <QDebug>
+
+#include <QImage>
+
+void exportimg(QImage image, QString filename)
+{
+    QFileInfo finfo(filename);
+    QFile file(filename+".c");
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+
+    QTextStream stream(&file);
+
+    stream << "__prog__ const unsigned short " << finfo.baseName() << "[] __attribute__((space(prog))) ={";
+
+    QImage mirored = image.mirrored(false,true);
+
+    for(int x=0; x<mirored.width(); x++)
+    {
+        stream << endl;
+        for(int y=0; y<mirored.height(); y++)
+        {
+            QRgb color = mirored.pixel(x,y);
+
+            unsigned short syscolor = 0;
+            syscolor |= (qRed(color)&0xF8)<<8;
+            syscolor |= (qGreen(color)&0xFC)<<3;
+            syscolor |= (qBlue(color)&0xF8)>>3;
+            stream << "0x" << QString::number(syscolor,16);
+            if(x!=mirored.width()-1 || y!=mirored.height()-1) stream << ", ";
+        }
+    }
+    stream << endl << "};";
+
+    file.close();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    
+    QString filename = QCoreApplication::arguments().at(1);
+    
+    /*QString filename = "../../../images_ecrans/new/Squaretips-smile-3.2.jpg";
+    QImage image(filename);
+    int ydec = (image.height()-(float)image.width()*320.0/480.0)/2.0;
+    qDebug()<<ydec;
+    QImage imageratio = image.copy(0,ydec,image.width(),image.height()-ydec*2);
+    QImage imagesc = imageratio.scaled(480,320,Qt::IgnoreAspectRatio,Qt::SmoothTransformation);
+
+    exportimg(imagesc, filename);
+
+    QImage zoeil1 = imagesc.copy(55,60,160,160);
+    exportimg(zoeil1, filename+"_zoeuil1");
+
+    QImage zoeil2 = imagesc.copy(267,60,160,160);
+    exportimg(zoeil2, filename+"_zoeuil2");
+
+    QImage bouche = imagesc.copy(190,220,110,90);
+    exportimg(bouche, filename+"_bouche");
+
+    ui->label->setPixmap(QPixmap::fromImage(imagesc));*/
+
+    /*QDir dir("../../../images_ecrans/new/out/");
+    foreach(QString file, dir.entryList(QStringList("*.jpg")))
+    {
+        qDebug()<<dir.path() + "/" + file;
+        QImage image(dir.path() + "/" + file);
+        exportimg(image, dir.path() + "/" + file.replace(".jpg",""));
+    }*/
+    
+    exportimg(QImage(filename), filename);
+}
---
+++ tool/img2raw/img2raw.pro
@@ -0,0 +1,11 @@
+QT += core gui
+
+CONFIG += c++11
+
+TARGET = img2raw
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += img2raw.cpp
