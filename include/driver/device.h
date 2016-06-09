57a4ff939f60b8fc8dd29afd911d703174207f66
sebcaux <sebastien.caux@robotips.fr>
2016-06-09 17:47:56 +0100

    Added new class device and introduced new type

--- include/driver/device.h
+++ include/driver/device.h
@@ -16,19 +16,20 @@
 
 // dev_t definition and macro helpers
 typedef uint16_t dev_t;
-#define MAJOR(dev)      ((uint8_t) ((dev) >> 8))
+typedef uint8_t dev_t_class;
+#define MAJOR(dev)      ((dev_t_class) ((dev) >> 8))
 #define MINOR(dev)      ((uint8_t) ((dev) & 0x00FF))
 #define MKDEV(ma,mi)    (((ma) << 8) | (mi))
-#define NULLDEV         (0)
+#define NULLDEV         ((dev_t_class) (0))
 
 // ===== device classes =====
 // base communication classes
-#define DEV_CLASS_UART          0x02
-#define DEV_CLASS_UART_SOFT     (DEV_CLASS_UART+1)
-#define DEV_CLASS_I2C           0x04
-#define DEV_CLASS_I2C_SOFT      (DEV_CLASS_I2C+1)
-#define DEV_CLASS_SPI           0x06
-#define DEV_CLASS_SPI_SOFT      (DEV_CLASS_SPI+1)
+#define DEV_CLASS_UART          ((dev_t_class) (0x02))
+#define DEV_CLASS_UART_SOFT     ((dev_t_class) (DEV_CLASS_UART+1))
+#define DEV_CLASS_I2C           ((dev_t_class) (0x04))
+#define DEV_CLASS_I2C_SOFT      ((dev_t_class) (DEV_CLASS_I2C+1))
+#define DEV_CLASS_SPI           ((dev_t_class) (0x06))
+#define DEV_CLASS_SPI_SOFT      ((dev_t_class) (DEV_CLASS_SPI+1))
 
 // special classes
 #define DEV_CLASS_QEI           0x11
