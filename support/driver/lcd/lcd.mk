35a36e83ca5e930c73f9cc5e71aec1de37994ca3
sebcaux <sebastien.caux@robotips.fr>
2016-06-20 23:07:18 +0100

    [lcd] Updated printf .mk

--- support/driver/lcd/lcd.mk
+++ support/driver/lcd/lcd.mk
@@ -18,20 +18,20 @@ endif
 # rule to build image to OUT_PWD/*.c
 $(OUT_PWD)/%.png.c : %.png pictures.h
 	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
-	@printf "IMG %-29s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
+	@printf "IMG %-30s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
 	@$(IMG2RAW_EXE) -i  $< -o  $(OUT_PWD)/$(notdir $@)
 $(OUT_PWD)/%.jpg.c : %.jpg pictures.h
 	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
-	@printf "IMG %-29s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
+	@printf "IMG %-30s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
 	@$(IMG2RAW_EXE) -i  $< -o  $(OUT_PWD)/$(notdir $@)
 $(OUT_PWD)/%.bmp.c : %.bmp pictures.h
 	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
-	@printf "IMG %-29s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
+	@printf "IMG %-30s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
 	@$(IMG2RAW_EXE) -i  $< -o  $(OUT_PWD)/$(notdir $@)
 
 # rule to build images *.<img>.c to OUT_PWD/*.o
 $(OUT_PWD)/%.o : $(OUT_PWD)/%.c
-	@printf "CC %-30s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
+	@printf "µCC %-30s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
 	@$(CC) $(CCFLAGS) -c $< $(DEFINES) $(INCLUDEPATH) -o $(OUT_PWD)/$(notdir $@)
 
 PICTURES_C := $(PICTURES)
