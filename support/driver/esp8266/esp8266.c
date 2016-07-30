69a8916bf93584ed7fa0524240c931489c1d2c3d
sebcaux <sebastien.caux@robotips.fr>
2016-07-30 15:18:29 +0100

    [esp8266] Refactoring code and adated to new uart send system

--- support/driver/esp8266/esp8266.c
+++ support/driver/esp8266/esp8266.c
@@ -9,9 +9,9 @@
 #include "driver/uart.h"
 
 // data/commands to send to esp
-uint16_t idSend=0;
+/*uint16_t idSend=0;
 uint16_t sizeSend=0;
-unsigned __attribute__((far)) char dataSend[2049];
+unsigned __attribute__((far)) char dataSend[2049];*/
 
 // data receive from esp
 uint8_t socket = 0;
@@ -28,426 +28,348 @@ rt_dev_t esp_uart;
 
 void esp_uart_init()
 {
-	// uart init
-	esp_uart = uart_getFreeDevice();
-	uart_setBaudSpeed(esp_uart, 115200);
-	uart_setBitConfig(esp_uart, 8, UART_BIT_PARITY_NONE, 1);
-	uart_enable(esp_uart);
-	
-	// Uart Interrupt
-	/*IPC2bits.U1RXIP = 6;		// Interrupt priority for receptor
-	IPC3bits.U1TXIP = 5;		// Interrupt priority for transmitor
-	IEC0bits.U1TXIE = 1;
-    IEC0bits.U1RXIE = 1;*/
+    // uart init
+    esp_uart = uart_getFreeDevice();
+    uart_setBaudSpeed(esp_uart, 115200);
+    uart_setBitConfig(esp_uart, 8, UART_BIT_PARITY_NONE, 1);
+    uart_enable(esp_uart);
+
+    // Uart Interrupt
+    /*IPC2bits.U1RXIP = 6;      // Interrupt priority for receptor
+    IPC3bits.U1TXIP = 5;        // Interrupt priority for transmitor*/
 }
 
 void esp_init()
 {
-	esp_uart_init();
+    esp_uart_init();
 }
 
-void esp_send_cmd(char data[])
+void esp_send_cmd(char cmd[])
 {
-	uint16_t i=0;
-	idSend=1;
-	
-	/*while(data[i]!=0)
-	{
-		dataSend[i]=data[i];
-		i++;
-	}
-	dataSend[i++]='\r';
-	dataSend[i++]='\n';
-	sizeSend=i;
-	
-	U1TXREG = data[0];
-	IEC0bits.U1TXIE = 1;*/
+    uart_write(esp_uart, cmd, strlen(cmd));
 }
-/*
-void __attribute__ ((interrupt,no_auto_psv)) _U1TXInterrupt(void)
-{
-	if(idSend<sizeSend)
-	{
-		U1TXREG = dataSend[idSend];
-		idSend++;
-	}
-	else
-	{
-		IEC0bits.U1TXIE = 0;
-	}	
-	IFS0bits.U1TXIF = 0;
-}*/
 
 /*
 void __attribute__ ((interrupt,no_auto_psv)) _U1RXInterrupt(void)
 {
-	char rec = U1RXREG;
-	
-	switch(wifistatus)
-	{
-		case FSM_UNKNOW:
-			if (rec == '\r')
-				wifistatus = FSM_WAITING_VALIDATE;
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_WAITING_VALIDATE:
-			if (rec == '\n')
-			{
-				//validate
-				if(pendingState!=WIFI_STATE_NONE) state = pendingState;
-				pendingState = WIFI_STATE_NONE;
-				
-				wifistatus = FSM_START;
-			}	
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_START:
-			if (rec == '\r')
-				wifistatus = FSM_WAITING_VALIDATE;
-			else if (rec == '+')
-				wifistatus = FSM_PLUS;
-			else if (rec == 'S')
-				wifistatus = FSM_SENDOK_S;
-			else if (rec == 'E')
-				wifistatus = FSM_ERROR_E;
-			else if (rec == '>')
-			{
-				state = WIFI_STATE_SEND_DATA;
-				wifistatus = FSM_UNKNOW;
-			}
-			else if (rec == 'O')
-				wifistatus = FSM_OK_O;
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-			
-		case FSM_SENDOK_S:
-			if (rec == 'E') wifistatus = FSM_SENDOK_E; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_SENDOK_E:
-			if (rec == 'N') wifistatus = FSM_SENDOK_N; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_SENDOK_N:
-			if (rec == 'D') wifistatus = FSM_SENDOK_D; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_SENDOK_D:
-			if (rec == ' ') wifistatus = FSM_SENDOK_Sp; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_SENDOK_Sp:
-			if (rec == 'O') wifistatus = FSM_SENDOK_O; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_SENDOK_O:
-			if (rec == 'K') wifistatus = FSM_SENDOK_K; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_SENDOK_K:
-			if (rec == '\r')
-			{
-				pendingState = WIFI_STATE_SEND_OK;
-				wifistatus = FSM_WAITING_VALIDATE;
-			}
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-			
-		case FSM_ERROR_E:
-			if (rec == 'R') wifistatus = FSM_ERROR_R1; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_ERROR_R1:
-			if (rec == 'R') wifistatus = FSM_ERROR_R2; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_ERROR_R2:
-			if (rec == 'O') wifistatus = FSM_ERROR_O; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_ERROR_O:
-			if (rec == 'R') wifistatus = FSM_ERROR_R3; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_ERROR_R3:
-			if (rec == '\r')
-			{
-				pendingState = WIFI_STATE_ERROR;
-				wifistatus = FSM_WAITING_VALIDATE;
-			}
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-			
-		case FSM_OK_O:
-			if (rec == 'K') wifistatus = FSM_OK_K; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_OK_K:
-			if (rec == '\r')
-			{
-				pendingState = WIFI_STATE_OK;
-				wifistatus = FSM_WAITING_VALIDATE;
-			}
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-			
-		case FSM_PLUS:
-			if (rec == 'I') wifistatus = FSM_IPD_I; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_IPD_I:
-			if (rec == 'P') wifistatus = FSM_IPD_P; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_IPD_P:
-			if (rec == 'D') wifistatus = FSM_IPD_D; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_IPD_D:
-			if (rec == ',') wifistatus = FSM_IPD_COMMA_1; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_IPD_COMMA_1:
-			if (rec >= '0' && rec <= '9')
-			{
-				wifistatus = FSM_IPD_SOCKET_DIGIT;
-				socket = rec - '0';
-			}	
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_IPD_SOCKET_DIGIT:
-			if (rec == ',') wifistatus = FSM_IPD_COMMA_2; else wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_IPD_COMMA_2:
-			if (rec >= '0' && rec <= '9')
-			{
-				wifistatus = FSM_IPD_SIZE_DIGITS;
-				sizeRecPacket = (rec - '0');
-			}	
-			else
-				wifistatus = FSM_UNKNOW;
-			break;
-		case FSM_IPD_SIZE_DIGITS:
-			if (rec == ':')
-			{
-				idRecPacket = 0;
-				wifistatus = FSM_PACKET_RX;
-			}
-			else
-			{
-				if (rec >= '0' && rec <= '9')
-				{
-					wifistatus = FSM_IPD_SIZE_DIGITS;
-					sizeRecPacket = sizeRecPacket * 10 + (rec - '0');
-				}	
-				else
-					wifistatus = FSM_UNKNOW;
-			}
-			break;
-		case FSM_PACKET_RX:
-			recPacket[idRecPacket++] = rec;
-			if(idRecPacket >= sizeRecPacket)
-			{
-				recPacketFlag=1;
-				state = WIFI_STATE_RECEIVE_DATA;
-				wifistatus = FSM_UNKNOW;
-			}
-			break;
-		default:
-			wifistatus = FSM_UNKNOW;
-	}
-	
-	IFS0bits.U1RXIF = 0;
+    char rec = U1RXREG;
+
+    switch(wifistatus)
+    {
+        case FSM_UNKNOW:
+            if (rec == '\r')
+                wifistatus = FSM_WAITING_VALIDATE;
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_WAITING_VALIDATE:
+            if (rec == '\n')
+            {
+                //validate
+                if(pendingState!=WIFI_STATE_NONE) state = pendingState;
+                pendingState = WIFI_STATE_NONE;
+
+                wifistatus = FSM_START;
+            }
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_START:
+            if (rec == '\r')
+                wifistatus = FSM_WAITING_VALIDATE;
+            else if (rec == '+')
+                wifistatus = FSM_PLUS;
+            else if (rec == 'S')
+                wifistatus = FSM_SENDOK_S;
+            else if (rec == 'E')
+                wifistatus = FSM_ERROR_E;
+            else if (rec == '>')
+            {
+                state = WIFI_STATE_SEND_DATA;
+                wifistatus = FSM_UNKNOW;
+            }
+            else if (rec == 'O')
+                wifistatus = FSM_OK_O;
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+
+        case FSM_SENDOK_S:
+            if (rec == 'E') wifistatus = FSM_SENDOK_E; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_SENDOK_E:
+            if (rec == 'N') wifistatus = FSM_SENDOK_N; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_SENDOK_N:
+            if (rec == 'D') wifistatus = FSM_SENDOK_D; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_SENDOK_D:
+            if (rec == ' ') wifistatus = FSM_SENDOK_Sp; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_SENDOK_Sp:
+            if (rec == 'O') wifistatus = FSM_SENDOK_O; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_SENDOK_O:
+            if (rec == 'K') wifistatus = FSM_SENDOK_K; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_SENDOK_K:
+            if (rec == '\r')
+            {
+                pendingState = WIFI_STATE_SEND_OK;
+                wifistatus = FSM_WAITING_VALIDATE;
+            }
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+
+        case FSM_ERROR_E:
+            if (rec == 'R') wifistatus = FSM_ERROR_R1; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_ERROR_R1:
+            if (rec == 'R') wifistatus = FSM_ERROR_R2; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_ERROR_R2:
+            if (rec == 'O') wifistatus = FSM_ERROR_O; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_ERROR_O:
+            if (rec == 'R') wifistatus = FSM_ERROR_R3; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_ERROR_R3:
+            if (rec == '\r')
+            {
+                pendingState = WIFI_STATE_ERROR;
+                wifistatus = FSM_WAITING_VALIDATE;
+            }
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+
+        case FSM_OK_O:
+            if (rec == 'K') wifistatus = FSM_OK_K; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_OK_K:
+            if (rec == '\r')
+            {
+                pendingState = WIFI_STATE_OK;
+                wifistatus = FSM_WAITING_VALIDATE;
+            }
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+
+        case FSM_PLUS:
+            if (rec == 'I') wifistatus = FSM_IPD_I; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_IPD_I:
+            if (rec == 'P') wifistatus = FSM_IPD_P; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_IPD_P:
+            if (rec == 'D') wifistatus = FSM_IPD_D; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_IPD_D:
+            if (rec == ',') wifistatus = FSM_IPD_COMMA_1; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_IPD_COMMA_1:
+            if (rec >= '0' && rec <= '9')
+            {
+                wifistatus = FSM_IPD_SOCKET_DIGIT;
+                socket = rec - '0';
+            }
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_IPD_SOCKET_DIGIT:
+            if (rec == ',') wifistatus = FSM_IPD_COMMA_2; else wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_IPD_COMMA_2:
+            if (rec >= '0' && rec <= '9')
+            {
+                wifistatus = FSM_IPD_SIZE_DIGITS;
+                sizeRecPacket = (rec - '0');
+            }
+            else
+                wifistatus = FSM_UNKNOW;
+            break;
+        case FSM_IPD_SIZE_DIGITS:
+            if (rec == ':')
+            {
+                idRecPacket = 0;
+                wifistatus = FSM_PACKET_RX;
+            }
+            else
+            {
+                if (rec >= '0' && rec <= '9')
+                {
+                    wifistatus = FSM_IPD_SIZE_DIGITS;
+                    sizeRecPacket = sizeRecPacket * 10 + (rec - '0');
+                }
+                else
+                    wifistatus = FSM_UNKNOW;
+            }
+            break;
+        case FSM_PACKET_RX:
+            recPacket[idRecPacket++] = rec;
+            if(idRecPacket >= sizeRecPacket)
+            {
+                recPacketFlag=1;
+                state = WIFI_STATE_RECEIVE_DATA;
+                wifistatus = FSM_UNKNOW;
+            }
+            break;
+        default:
+            wifistatus = FSM_UNKNOW;
+    }
+
+    IFS0bits.U1RXIF = 0;
 }*/
+
+char * esp_strcat_protected(char * destination, const char * source)
+{
+    char *ptr, *ptrsource;
+    ptr = destination + strlen(destination);
+    ptrsource = (char *)source;
+
+    while(*ptrsource!=0)
+    {
+        if(*ptrsource=='"' || *ptrsource==',' || *ptrsource=='\\') (*(ptr++))='\\';
+        (*(ptr++))=*ptrsource;
+        ptrsource++;
+    }
+    return destination;
+}
 
 void wait_ok()
 {
-	while(state != WIFI_STATE_OK);
-	state = WIFI_STATE_NONE;
+    while(state != WIFI_STATE_OK);
+    state = WIFI_STATE_NONE;
 }
 
 WIFI_STATE get_state()
 {
-	return state;
+    return state;
 }
 
 uint8_t getRecSocket()
 {
-	return socket;
+    return socket;
 }
 
 char *getRecData()
 {
-	return recPacket;
+    return recPacket;
 }
 
 uint16_t getRecSize()
 {
-	return sizeRecPacket;
+    return sizeRecPacket;
 }
 
 uint8_t getRec()
 {
-	if(recPacketFlag==1)
-	{
-		recPacketFlag=0;
-		return 1;
-	}
-	return 0;
+    if(recPacketFlag==1)
+    {
+        recPacketFlag=0;
+        return 1;
+    }
+    return 0;
 }
 
-uint8_t esp_open_tcp_socket(char ip_domain[], uint16_t port)
+uint8_t esp_open_tcp_socket(char *ip_domain, uint16_t port)
 {
-	esp_send_cmd("AT+CIPSTART=0,\"TCP\",\"robotips.fr\",80");
-	wait_ok();
-	return 0;
+    // TODO implement with args
+    esp_send_cmd("AT+CIPSTART=0,\"TCP\",\"robotips.fr\",80\r\n");
+    wait_ok();
+    return 0;
 }
 
-uint8_t esp_open_udp_socket(char ip_domain[], uint16_t port, uint16_t localPort, uint8_t mode)
+uint8_t esp_open_udp_socket(char *ip_domain, uint16_t port, uint16_t localPort, uint8_t mode)
 {
-	return 0;
+    // TODO implement me
+    return 0;
 }
 
 void esp_set_mode(ESP_MODE mode)
 {
-	char buff[20];
-	char *ptbuff=buff;
-	
-	(*(ptbuff++))='A';
-	(*(ptbuff++))='T';
-	(*(ptbuff++))='+';
-	(*(ptbuff++))='C';
-	(*(ptbuff++))='W';
-	(*(ptbuff++))='M';
-	(*(ptbuff++))='O';
-	(*(ptbuff++))='D';
-	(*(ptbuff++))='E';
-	(*(ptbuff++))='=';
-	
-	if(mode==ESP_MODE_AP) (*(ptbuff++))='2';
-	else if(mode==ESP_MODE_STA_AP) (*(ptbuff++))='3';
-	else (*(ptbuff++))='1';
-	
-	(*(ptbuff++))='\0';
-	esp_send_cmd(buff);
-	wait_ok();
+    char buff[20];
+    strcpy(buff, "AT+CWMODE=");
+
+    if(mode==ESP_MODE_AP) strcat(buff, "2");
+    else if(mode==ESP_MODE_STA_AP) strcat(buff, "3");
+    else strcat(buff, "1");
+
+    strcat(buff, "\r\n");
+
+    esp_send_cmd(buff);
+    wait_ok();
 }
 
-uint8_t esp_connect_ap(char ssid[], char pw[])		// warning, be careful to special car, protect with backslash
+uint8_t esp_connect_ap(char *ssid, char *pw)        // warning, be careful to special car, protect with backslash
 {
-	char buff[100];
-	char *ptbuff=buff;
-	char *ptssid=ssid;
-	char *ptpw=pw;
-	
-	(*(ptbuff++))='A';
-	(*(ptbuff++))='T';
-	(*(ptbuff++))='+';
-	(*(ptbuff++))='C';
-	(*(ptbuff++))='W';
-	(*(ptbuff++))='J';
-	(*(ptbuff++))='A';
-	(*(ptbuff++))='P';
-	(*(ptbuff++))='=';
-	
-	(*(ptbuff++))='"';
-	while(*ptssid!=0)
-	{
-		if(*ptssid=='"' || *ptssid==',' || *ptssid=='\\') (*(ptbuff++))='\\';
-		(*(ptbuff++))=*ptssid;
-		ptssid++;
-	}
-	(*(ptbuff++))='"';
-	(*(ptbuff++))=',';
-	
-	(*(ptbuff++))='"';
-	while(*ptpw!=0)
-	{
-		if(*ptpw=='"' || *ptpw==',' || *ptpw=='\\') (*(ptbuff++))='\\';
-		(*(ptbuff++))=*ptpw;
-		ptpw++;
-	}
-	(*(ptbuff++))='"';
-	
-	(*(ptbuff++))='\0';
-	esp_send_cmd(buff);
-	wait_ok();
-	
-	return 0;
+    char buff[100];
+
+    strcpy(buff, "AT+CWJAP=\"");
+    esp_strcat_protected(buff, ssid);
+    strcat(buff, "\",\"");
+    esp_strcat_protected(buff, ssid);
+    strcat(buff, "\"\r\n");
+
+    esp_send_cmd(buff);
+    wait_ok();
+    return 0;
 }
 
 uint8_t esp_disconnect_ap()
 {
-	esp_send_cmd("AT+CWQAP");
-	wait_ok();
-	return 0;
+    esp_send_cmd("AT+CWQAP\r\n");
+    wait_ok();
+    return 0;
 }
 
 void esp_close_socket(uint8_t sock)
 {
-	char buff[20];
-	char *ptbuff=buff;
-	
-	(*(ptbuff++))='A';
-	(*(ptbuff++))='T';
-	(*(ptbuff++))='+';
-	(*(ptbuff++))='C';
-	(*(ptbuff++))='I';
-	(*(ptbuff++))='P';
-	(*(ptbuff++))='C';
-	(*(ptbuff++))='L';
-	(*(ptbuff++))='O';
-	(*(ptbuff++))='S';
-	(*(ptbuff++))='E';
-	(*(ptbuff++))='=';
-	(*(ptbuff++))=sock+'0';
-	(*(ptbuff++))='\0';
-	esp_send_cmd(buff);
-	wait_ok();
+    char buff[20];
+    strcpy(buff, "AT+CIPCLOSE=%\r\n");
+    (*strstr(buff, "%")) = sock + '0';
+    esp_send_cmd(buff);
+    wait_ok();
 }
 
-void esp_write_socket(uint8_t sock, char data[], uint16_t size)
+void esp_write_socket(uint8_t sock, char *data, uint16_t size)
 {
-	char buff[20];
-	char *ptbuff=buff;
-	
-	(*(ptbuff++))='A';
-	(*(ptbuff++))='T';
-	(*(ptbuff++))='+';
-	(*(ptbuff++))='C';
-	(*(ptbuff++))='I';
-	(*(ptbuff++))='P';
-	(*(ptbuff++))='S';
-	(*(ptbuff++))='E';
-	(*(ptbuff++))='N';
-	(*(ptbuff++))='D';
-	(*(ptbuff++))='=';
-	(*(ptbuff++))=sock+'0';
-	(*(ptbuff++))=',';
-	
-	//itoa(ptbuff, size, 10);
-	
-	esp_send_cmd(buff);
-	
-	while(state != WIFI_STATE_SEND_DATA);
-	state = WIFI_STATE_NONE;
-	
-	esp_write(data, size);
-	while(state != WIFI_STATE_SEND_OK);
+    char buff[20];
+    strcpy(buff, "AT+CIPSEND=%,");
+    (*strstr(buff, "%")) = sock + '0';
+    itoa(buff + strlen(buff), size, 10);
+    strcat(buff, "\r\n");
+    esp_send_cmd(buff);
+
+    while(state != WIFI_STATE_SEND_DATA);
+    state = WIFI_STATE_NONE;
+
+    esp_write(data, size);
+    while(state != WIFI_STATE_SEND_OK);
 }
 
-void esp_write_socket_string(uint8_t sock, char str[])
+void esp_write_socket_string(uint8_t sock, char *str)
 {
-	uint16_t size=0;
-	
-	while(str[size]!=0) size++;
-	esp_write_socket(sock, str, size);
+    uint16_t size=0;
+
+    while(str[size]!=0) size++;
+    esp_write_socket(sock, str, size);
 }
 
-void esp_write(char data[], uint16_t size)
+void esp_write(char *data, uint16_t size)
 {
-	uint16_t i=0;
-	idSend=1;
-	
-	while(i<size)
-	{
-		dataSend[i]=data[i];
-		i++;
-	}
-	sizeSend=size;
-	
-	/*U1TXREG = data[0];
-	IEC0bits.U1TXIE = 1;*/
+    /*uint16_t i=0;
+    idSend=1;
+
+    while(i<size)
+    {
+        dataSend[i]=data[i];
+        i++;
+    }
+    sizeSend=size;*/
+
+    /*U1TXREG = data[0];
+    IEC0bits.U1TXIE = 1;*/
 }
--- support/driver/esp8266/esp8266.h
+++ support/driver/esp8266/esp8266.h
@@ -13,67 +13,67 @@ void esp_rst();
 // wifi layer
 typedef enum
 {
-	ESP_MODE_STA = 0,
-	ESP_MODE_AP,
-	ESP_MODE_STA_AP
+    ESP_MODE_STA = 0,
+    ESP_MODE_AP,
+    ESP_MODE_STA_AP
 } ESP_MODE;
 void esp_set_mode(ESP_MODE mode);
-uint8_t esp_connect_ap(char ssid[], char pw[]);		// warning, be careful to special car, protect with backslash
+uint8_t esp_connect_ap(char *ssid, char *pw);       // warning, be careful to special car, protect with backslash
 uint8_t esp_disconnect_ap();
 
 // tcp/ip layer
-uint8_t esp_open_tcp_socket(char ip_domain[], uint16_t port);
-uint8_t esp_open_udp_socket(char ip_domain[], uint16_t port, uint16_t localPort, uint8_t mode);
+uint8_t esp_open_tcp_socket(char *ip_domain, uint16_t port);
+uint8_t esp_open_udp_socket(char *ip_domain, uint16_t port, uint16_t localPort, uint8_t mode);
 void esp_close_socket(uint8_t sock);
-void esp_write_socket(uint8_t sock, char data[], uint16_t size);
-void esp_write_socket_string(uint8_t sock, char str[]);
+void esp_write_socket(uint8_t sock, char *data, uint16_t size);
+void esp_write_socket_string(uint8_t sock, char *str);
 
-typedef enum 
+typedef enum
 {
-	FSM_UNKNOW = 0,
-	FSM_WAITING_VALIDATE,
-	FSM_START,
-	
-	FSM_OK_O,
-	FSM_OK_K,
-	
-	FSM_SENDOK_S,
-	FSM_SENDOK_E,
-	FSM_SENDOK_N,
-	FSM_SENDOK_D,
-	FSM_SENDOK_Sp,
-	FSM_SENDOK_O,
-	FSM_SENDOK_K,
-	
-	FSM_ERROR_E,
-	FSM_ERROR_R1,
-	FSM_ERROR_R2,
-	FSM_ERROR_O,
-	FSM_ERROR_R3,
-	
-	FSM_PLUS,
-	
-	FSM_IPD_I,
-	FSM_IPD_P,
-	FSM_IPD_D,
-	FSM_IPD_COMMA_1,
-	FSM_IPD_SOCKET_DIGIT,
-	FSM_IPD_COMMA_2,
-	FSM_IPD_SIZE_DIGITS,
-	FSM_PACKET_RX,
-	FSM_RX_COMPLETE	
-	
+    FSM_UNKNOW = 0,
+    FSM_WAITING_VALIDATE,
+    FSM_START,
+
+    FSM_OK_O,
+    FSM_OK_K,
+
+    FSM_SENDOK_S,
+    FSM_SENDOK_E,
+    FSM_SENDOK_N,
+    FSM_SENDOK_D,
+    FSM_SENDOK_Sp,
+    FSM_SENDOK_O,
+    FSM_SENDOK_K,
+
+    FSM_ERROR_E,
+    FSM_ERROR_R1,
+    FSM_ERROR_R2,
+    FSM_ERROR_O,
+    FSM_ERROR_R3,
+
+    FSM_PLUS,
+
+    FSM_IPD_I,
+    FSM_IPD_P,
+    FSM_IPD_D,
+    FSM_IPD_COMMA_1,
+    FSM_IPD_SOCKET_DIGIT,
+    FSM_IPD_COMMA_2,
+    FSM_IPD_SIZE_DIGITS,
+    FSM_PACKET_RX,
+    FSM_RX_COMPLETE
+
 } WIFIstatus;
 
 typedef enum
 {
-	WIFI_STATE_NONE = 0,
-	WIFI_STATE_OK,
-	WIFI_STATE_ERROR,
-	WIFI_STATE_RECEIVE_DATA,
-	WIFI_STATE_SEND_DATA,
-	WIFI_STATE_SEND_OK
-	
+    WIFI_STATE_NONE = 0,
+    WIFI_STATE_OK,
+    WIFI_STATE_ERROR,
+    WIFI_STATE_RECEIVE_DATA,
+    WIFI_STATE_SEND_DATA,
+    WIFI_STATE_SEND_OK
+
 } WIFI_STATE;
 
 void wait_ok();
