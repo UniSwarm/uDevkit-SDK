b2c511b553b84b073fb1ba3150b7a659aef49d4a
sebcaux <sebastien.caux@robotips.fr>
2016-08-20 11:23:08 +0100

    [type.h] Protect definition against multiple definition

--- include/sys/types.h
+++ include/sys/types.h
@@ -13,13 +13,19 @@
 
 #include <stdint.h>
 
-#ifndef _SIZE_T_DEFINED
+#if !defined(_SIZE_T_DEFINED) && !defined(_SIZE_T) && !defined(_SIZET)&& !defined(_BSD_SIZE_T_DEFINED_)
 #define _SIZE_T_DEFINED
+#define _SIZE_T
+#define _SIZET
+#define _BSD_SIZE_T_DEFINED_
  typedef uint16_t size_t;
 #endif
 
-#ifndef _SSIZE_T_DEFINED
+#if !defined(_SSIZE_T_DEFINED) && !defined(_SSIZE_T) && !defined(_SSIZET)&& !defined(_BSD_SSIZE_T_DEFINED_)
 #define _SSIZE_T_DEFINED
+#define _SSIZE_T
+#define _SSIZET
+#define _BSD_SSIZE_T_DEFINED_
  typedef int16_t ssize_t;
 #endif
 
