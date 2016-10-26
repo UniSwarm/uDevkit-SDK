5eee116493c7de609b3bdf7d2c8878312fc699b5
sebcaux <sebastien.caux@robotips.fr>
2016-10-26 16:50:02 +0100

    [microchip] Added debug rules to see implemented symbols and view disably listing

--- support/archi/microchip/microchip.mk
+++ support/archi/microchip/microchip.mk
@@ -21,7 +21,7 @@ hex : $(OUT_PWD)/$(PROJECT).hex
 $(OUT_PWD)/%.o : %.c
 	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
 	@printf "µCC %-35s => %s\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
-	@$(CC) -mcpu=$(DEVICE) $(CCFLAGS) $(CCFLAGS_XC) -c  $< $(DEFINES) $(INCLUDEPATH) -o  $(OUT_PWD)/$(notdir $@)
+	$(CC) -mcpu=$(DEVICE) $(CCFLAGS) $(CCFLAGS_XC) -c  $< $(DEFINES) $(INCLUDEPATH) -o  $(OUT_PWD)/$(notdir $@)
 	@$(CC) -mcpu=$(DEVICE) $(CCFLAGS) $(CCFLAGS_XC) -MM $< $(DEFINES) $(INCLUDEPATH) -MT $(OUT_PWD)/$(notdir $@) > $(OUT_PWD)/$*.d
 
 # rule to link OBJECTS to an elf in OUT_PWD
@@ -29,6 +29,11 @@ $(OUT_PWD)/$(PROJECT).elf : $(OBJECTS)
 	@printf "µLD %-35s => %s\n" "*.o" $(OUT_PWD)/$(PROJECT).elf
 	@$(CC) $(CCFLAGS) $(CCFLAGS_XC) -o $(OUT_PWD)/$(PROJECT).elf $(addprefix $(OUT_PWD)/,$(notdir $(OBJECTS))) -lc -T p$(DEVICE).gld
 
-.PHONY : showmem
+.PHONY : showmem dbg.% dbg
 showmem :
 	@$(CC) $(CCFLAGS) $(CCFLAGS_XC) -o $(OUT_PWD)/$(PROJECT).elf $(addprefix $(OUT_PWD)/,$(notdir $(OBJECTS))) -lc -T p$(DEVICE).gld -Wl,--report-mem
+
+dbg.% : $(OUT_PWD)/$(PROJECT).elf
+	xc16-objdump -S $(OUT_PWD)/$(PROJECT).elf |sed -n -e '/<_*$*>/,/<_.*>/ p'
+dbg : $(OUT_PWD)/$(PROJECT).elf
+	xc16-objdump -t $(OUT_PWD)/$(PROJECT).elf |grep "F .text" |sort -k6
