750898a1ce5daf7732762843152ac39cd36f6d04
sebcaux <sebastien.caux@robotips.fr>
2016-10-29 23:20:42 +0100

    [cmdline] cmd_uart added command read/write and setbaudspeed

--- support/module/cmdline/cmd/cmd_uart.c
+++ support/module/cmdline/cmd/cmd_uart.c
@@ -3,6 +3,7 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 #define puts(str) cmd_puts((str))
 #define printf(format, ...) cmd_printf((format), ##__VA_ARGS__)
@@ -41,7 +42,7 @@ int cmd_uart(int argc, char **argv)
     }
     uart_dev = MKDEV(DEV_CLASS_UART, uart);
 
-    // if no more arg, print property of uart
+    // if no more arg, print properties of uart
     if(argc == 2)
     {
         char parity;
@@ -67,6 +68,36 @@ int cmd_uart(int argc, char **argv)
             (int)uart_bitStop(uart_dev),
             uart_baudSpeed(uart_dev));
 
+        return 0;
+    }
+
+    // parse argv 2
+
+    if(argc < 4)
+        return 1;
+    // == write
+    if(strcmp(argv[2], "write")==0)
+    {
+        uart_write(uart_dev, argv[3], strlen(argv[3]));
+        puts("ok");
+        return 0;
+    }
+    // == read
+    if(strcmp(argv[2], "read")==0)
+    {
+        char buff[100];
+        size_t data_read;
+        data_read = uart_read(uart_dev, buff, 100);
+        buff[data_read] = 0;
+        puts(buff);
+        return 0;
+    }
+    // == setbs
+    if(strcmp(argv[2], "setbs")==0)
+    {
+        uint32_t baudSpeed;
+        baudSpeed = atol(argv[3]);
+        uart_setBaudSpeed(uart_dev, baudSpeed);
         return 0;
     }
 
