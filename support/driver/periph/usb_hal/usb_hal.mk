7450c1c62c3192372d3a075adb0c9553ab1b5286
sebcaux <sebastien.caux@robotips.fr>
2016-11-10 23:43:10 +0000

    [usb_serial] Fixed mk file with src moved to archi_src

--- support/driver/periph/usb_hal/usb_hal.mk
+++ support/driver/periph/usb_hal/usb_hal.mk
@@ -7,17 +7,17 @@ INCLUDEPATH:= $(INCLUDEPATH) -I$(DRIVERPATH)
 ifeq ($(ARCHI), $(filter $(ARCHI),pic24ep pic24fj dspic33ep))
  vpath %.c $(RTPROG)/contrib/microchip/mla_usb/src
  INCLUDEPATH:= $(INCLUDEPATH) -I$(RTPROG)/contrib/microchip/mla_usb/inc
- SRC += usb_device.c usb_device_generic.c system.c
+ ARCHI_SRC += usb_device.c usb_device_generic.c system.c
 endif
 
 ifeq ($(ARCHI), pic24ep)
- SRC += usb_hal_pic24e.c
+ ARCHI_SRC += usb_hal_pic24e.c
 endif
 ifeq ($(ARCHI), pic24fj)
- SRC += usb_hal_pic24f.c
+ ARCHI_SRC += usb_hal_pic24f.c
 endif
 ifeq ($(ARCHI), dspic33ep)
- SRC += usb_hal_dspic33e.c
+ ARCHI_SRC += usb_hal_dspic33e.c
 endif
 
 endif
