536386a0d05ef90adbd84522b51fe78dabe9ded2
sebcaux <sebastien.caux@robotips.fr>
2016-11-10 23:40:51 +0000

    New driver scrip updated and fixed

--- support/driver/newdriver.sh
+++ support/driver/newdriver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then 
+if [ -z "$1" ]; then
 	echo "You need to specify the name"
 	exit
 fi
 
-if [ -z "$2" ]; then 
+if [ -z "$2" ]; then
 	echo "You need to specify a description as param 2"
 	exit
 fi
@@ -29,8 +29,8 @@ echo -e "" >> ${FILENAME}
 echo -e "HEADER += ${DRIVER}.h" >> ${FILENAME}
 echo -e "" >> ${FILENAME}
 echo -e "ifeq (\$(ARCHI),\$(filter \$(ARCHI),pic24ep pic24f pic24fj pic24hj dspic30f dspic33ep dspic33ev dspic33fj))" >> ${FILENAME}
-echo -e " ARCHI_SRC += ${DRIVER}_pic24dspic33.c" >> ${FILENAME}
-echo -e " HEADER += ${DRIVER}_pic24dspic33.h" >> ${FILENAME}
+echo -e " ARCHI_SRC += ${DRIVER}_pic24_dspic30_dspic33.c" >> ${FILENAME}
+echo -e " HEADER += ${DRIVER}_pic24_dspic30_dspic33.h" >> ${FILENAME}
 echo -e "endif" >> ${FILENAME}
 echo -e "" >> ${FILENAME}
 echo -e "endif" >> ${FILENAME}
@@ -52,41 +52,52 @@ echo -e "#define ${DRIVER_UPPER}_H" >> ${FILENAME}
 echo -e "" >> ${FILENAME}
 echo -e "#include \"driver/device.h\"" >> ${FILENAME}
 echo -e "" >> ${FILENAME}
-echo -e "#if defined(ARCHI_pic24ep) || defined(ARCHI_pic24f) defined(ARCHI_pic24fj) || defined(ARCHI_pic24fv) || defined(ARCHI_pic24hj) \\" >> ${FILENAME}
-echo -e " || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev) defined(ARCHI_dspic33fj)" >> ${FILENAME}
-echo -e " #include \"${DRIVER}_pic24dspic33.h\"" >> ${FILENAME}
+
+echo -e "// ====== device assignation ======" >> ${FILENAME}
+echo -e "rt_dev_t ${DRIVER}_getFreeDevice();" >> ${FILENAME}
+echo -e "void ${DRIVER}_releaseDevice(rt_dev_t device);" >> ${FILENAME}
+echo -e "" >> ${FILENAME}
+echo -e "// ==== device enable/disable =====" >> ${FILENAME}
+echo -e "int ${DRIVER}_enable(rt_dev_t device);" >> ${FILENAME}
+echo -e "int ${DRIVER}_disable(rt_dev_t device);" >> ${FILENAME}
+
+echo -e "" >> ${FILENAME}
+echo -e "" >> ${FILENAME}
+echo -e "#if defined(ARCHI_pic24ep) || defined(ARCHI_pic24f) || defined(ARCHI_pic24fj) || defined(ARCHI_pic24fv) || defined(ARCHI_pic24hj) \\" >> ${FILENAME}
+echo -e " || defined(ARCHI_dspic30f) || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev) || defined(ARCHI_dspic33fj)" >> ${FILENAME}
+echo -e " #include \"${DRIVER}_pic24_dspic30_dspic33.h\"" >> ${FILENAME}
 echo -e "#else" >> ${FILENAME}
 echo -e " #error Unsuported ARCHI" >> ${FILENAME}
 echo -e "#endif" >> ${FILENAME}
 echo -e "" >> ${FILENAME}
-echo -e "" >> ${FILENAME}
-echo -e "" >> ${FILENAME}
 echo -e "#endif // ${DRIVER_UPPER}_H" >> ${FILENAME}
 
 # _dspic.c
-FILENAME=periph/${DRIVER}/${DRIVER}_pic24dspic33.c
+FILENAME=periph/${DRIVER}/${DRIVER}_pic24_dspic30_dspic33.c
 echo -e "/**" > ${FILENAME}
-echo -e " * @file ${DRIVER}_pic24dspic33.c" >> ${FILENAME}
+echo -e " * @file ${DRIVER}_pic24_dspic30_dspic33.c" >> ${FILENAME}
 echo -e " * @author Sebastien CAUX (sebcaux)" >> ${FILENAME}
 echo -e " * @copyright Robotips 2016" >> ${FILENAME}
 echo -e " *" >> ${FILENAME}
 echo -e " * @date ${DATE} " >> ${FILENAME}
 echo -e " * " >> ${FILENAME}
-echo -e " * @brief ${DESC}" >> ${FILENAME}
+echo -e " * @brief ${DESC} for dsPIC30F, dsPIC33FJ, dsPIC33EP, dsPIC33EV," >> ${FILENAME}
+echo -e " * PIC24F, PIC24FJ, PIC24EP and PIC24HJ" >> ${FILENAME}
 echo -e " */" >> ${FILENAME}
 echo -e "" >> ${FILENAME}
 echo -e "#include \"${DRIVER}.h\"" >> ${FILENAME}
 
 # _dspic.h
-FILENAME=periph/${DRIVER}/${DRIVER}_pic24dspic33.h
+FILENAME=periph/${DRIVER}/${DRIVER}_pic24_dspic30_dspic33.h
 echo -e "/**" > ${FILENAME}
-echo -e " * @file ${DRIVER}_dspic.h" >> ${FILENAME}
+echo -e " * @file ${DRIVER}_pic24_dspic30_dspic33.h" >> ${FILENAME}
 echo -e " * @author Sebastien CAUX (sebcaux)" >> ${FILENAME}
 echo -e " * @copyright Robotips 2016" >> ${FILENAME}
 echo -e " *" >> ${FILENAME}
 echo -e " * @date ${DATE} " >> ${FILENAME}
 echo -e " * " >> ${FILENAME}
-echo -e " * @brief ${DESC}" >> ${FILENAME}
+echo -e " * @brief ${DESC} for dsPIC30F, dsPIC33FJ, dsPIC33EP, dsPIC33EV," >> ${FILENAME}
+echo -e " * PIC24F, PIC24FJ, PIC24EP and PIC24HJ" >> ${FILENAME}
 echo -e " */" >> ${FILENAME}
 echo -e "" >> ${FILENAME}
 echo -e "#ifndef ${DRIVER_UPPER}_DSPIC_H" >> ${FILENAME}
