67d8affe03d6f189f4c5a7ac30b1e0a46531777a
sebcaux <sebastien.caux@robotips.fr>
2016-11-22 23:17:32 +0100

    [mrobot] Added missing header

--- support/module/mrobot/mrobot.h
+++ support/module/mrobot/mrobot.h
@@ -11,8 +11,55 @@
 #ifndef MROBOT_H
 #define MROBOT_H
 
-#include "asserv/asserv.h"
+#include "driver/device.h"
+
+//#include "asserv/asserv.h"
 
 void mrobot_init();
+
+typedef struct
+{
+    float x;
+    float y;
+} MrobotPoint;
+
+typedef struct
+{
+    float x;
+    float y;
+    float t;
+} MrobotPose;
+
+// ======== settings ========
+// coders, geometry, location
+void mrobot_setCoderDev(rt_dev_t leftCoder_dev, rt_dev_t rightCoder_dev);
+void mrobot_setCoderWay(uint8_t leftCoder_way, uint8_t rightCoder_way);
+void mrobot_setCoderGeometry(float entrax, float stepLenght);
+void mrobot_setPose(MrobotPose pose);
+
+// motors control
+void mrobot_setMotorDev(rt_dev_t leftMotor_dev, rt_dev_t rightMotor_dev);
+void mrobot_setMotorWay(uint8_t leftMotor_way, uint8_t rightMotor_way);
+void mrobot_setMotorPid(int16_t kp, int16_t ki, int16_t kd);
+int16_t mrobot_motorGetP();
+int16_t mrobot_motorGetI();
+int16_t mrobot_motorGetD();
+
+// ===== pos / movements =====
+// commands
+void mrobot_pause();
+void mrobot_restart();
+void mrobot_stop();
+void mrobot_goto(MrobotPoint pos, int16_t speed);        // global reference frame
+void mrobot_move(MrobotPoint movement, int16_t speed);   // local (robot) reference frame
+MrobotPoint mapToGlobal(MrobotPoint pos);
+MrobotPoint mapToLocal(MrobotPoint pos);
+
+// status
+MrobotPose mrobot_pose();
+MrobotPoint mrobot_nextKeypoint();
+float mrobot_nextKeypointDistance();
+float mrobot_speed();
+
 
 #endif // MROBOT_H
