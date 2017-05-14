21e2af09d940c8af47bc66bb33e0c28667fb13a6
sebcaux <sebastien.caux@robotips.fr>
2017-05-14 12:21:21 +0100

    [swarm2test] Added coder interface capabilities

--- test/swarm2test/main.c
+++ test/swarm2test/main.c
@@ -6,58 +6,75 @@
 #include "board.h"
 #include "archi.h"
 
+#include "ihm.h"
+
 extern int ihm_d1, ihm_d2, ihm_d3;
+extern int ihm_c1, ihm_c2;
 extern float ihm_batt;
+
+int c1 = 0, c2 = 0;
+int8_t c1_state = 0, c2_state = 0;
+const signed char encoder_value[16] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+void process_coder()
+{
+	// c1_state => 0b0000 & previous_A & previous_B & A & B
+	c1_state = (c1_state<<2) & 0b00001100;
+	if (C1A == 1)
+        c1_state+=2;
+	if (C1B == 1)
+        c1_state+=1;
+	c1 += encoder_value[c1_state];
+    ihm_c1 = c1;
+
+	// c2_state => 0b0000 & previous_A & previous_B & A & B
+	c2_state = (c2_state<<2) & 0b00001100;
+	if (C2A == 1)
+        c2_state+=2;
+	if (C2B == 1)
+        c2_state+=1;
+	c2 += encoder_value[c2_state];
+    ihm_c2 = c2;
+}
 
 int main(void)
 {
     unsigned int i, j;
     int16_t value;
-    
+
     init_archi();
     board_init();
 
     // module init
     network_init();
 
-    for(j=0;j<5;j++) for(i=0;i<65000;i++);
+    for (j=0;j<5;j++)
+        for (i=0;i<65000;i++);
     value = sysclock_source();
     board_setLed(1, 1);
 
     // screen test
-
     ihm_init();
 
+    // timer coders
+    rt_dev_t timer;
+    timer = timer_getFreeDevice();
+    timer_setPeriodMs(timer, 1);
+    timer_setHandler(timer, process_coder);
+    timer_enable(timer);
+
     // motors test
-    /*PR3 = 512; // Set period
-    T3CON = 0x8020;// Enable Timer3
-
-    OC3CON = 0x0000;// Turn off the OC3 when performing the setup
-    OC3R = 0;// Initialize primary Compare register
-    OC3RS = 0;// Initialize secondary Compare register
-    OC3CON = 0x0006;// Configure for PWM mode without Fault pin // enabled
-    OC3CONbits.OCTSEL = 1; // timer y
-    OC3CONSET = 0x8000;// Enable OC3
-
-    OC4CON = 0x0000;// Turn off the OC4 when performing the setup
-    OC4R = 0;// Initialize primary Compare register
-    OC4RS = 0;// Initialize secondary Compare register
-    OC4CON = 0x0006;// Configure for PWM mode without Fault pin // enabled
-    OC4CONbits.OCTSEL = 1; // timer y
-    OC4CONSET = 0x8000;// Enable OC4*/
-    
     rt_dev_t pwm_m1 = pwm(3);
     pwm_open(pwm_m1);
     oc_setTimer(oc(3), 1);
     pwm_setFreq(pwm_m1, 20000);
     pwm_enable(pwm_m1);
-    
+
     rt_dev_t pwm_m2 = pwm(4);
     pwm_open(pwm_m2);
     oc_setTimer(oc(4), 1);
     pwm_setFreq(pwm_m2, 20000);
     pwm_enable(pwm_m2);
-    
+
 #ifndef SIMULATOR
     BOOST_SLEEP = 0;
     M1DIR = 1;
