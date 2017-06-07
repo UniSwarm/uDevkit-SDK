d248f62d9fd64cf5bf4378e3aa98d2e429772a82
sebcaux <sebastien.caux@robotips.fr>
2017-06-07 17:46:43 +0200

    [img2raw] Fixed invalid image format, added verbosity and reformated file

--- tool/img2raw/img2raw.cpp
+++ tool/img2raw/img2raw.cpp
@@ -5,7 +5,8 @@
  *
  * @date April 25, 2016, 18:39 PM
  *
- * @brief Tool to transform image in const C raw data (xc16 format) and storing its metadata
+ * @brief Tool to transform image in const C raw data (xc16 format) and storing
+ * its metadata
  */
 
 #include <QCommandLineParser>
@@ -25,10 +26,11 @@
 #include <QRegExp>
 
 /**
- * @brief exportImage convert an image to a structure containing metadata and data
- * To read the arguments list of the Picture struc, see "/module/gui.gui.h" 
+ * @brief exportImage convert an image to a structure containing metadata and
+ * data
+ * To read the arguments list of the Picture struc, see "/module/gui.gui.h"
  */
-void exportImage(const QImage& image, const QString& filename)
+void exportImage(const QImage &image, const QString &filename)
 {
     QFileInfo finfo(filename);
     QFile file(filename);
@@ -36,36 +38,39 @@ void exportImage(const QImage& image, const QString& filename)
 
     QTextStream stream(&file);
 
-    //starting preprocessor instructions
+    // starting preprocessor instructions
     stream << "#include <gui/picture.h>" << endl;
     stream << endl;
 
-    //creating an array containnig the image data
-    stream << "//an array containnig the image data" << endl;
-    stream << "__prog__ const uint16_t " << finfo.baseName() << "_data[] __space_prog__ = {";
+    // creating an array containnig the image data
+    stream << "// an array containnig the image data" << endl;
+    stream << "__prog__ const uint16_t " << finfo.baseName()
+           << "_data[] __space_prog__ = {";
 
-    QImage mirrored = image.mirrored(false,false);
+    QImage mirrored = image.mirrored(false, false);
 
-    for(int x=0; x<mirrored.width(); ++x)
+    for (int x = 0; x < mirrored.width(); ++x)
     {
         stream << endl;
-        for(int y=0; y<mirrored.height(); ++y)
+        for (int y = 0; y < mirrored.height(); ++y)
         {
-            QRgb color = mirrored.pixel(x,y);
+            QRgb color = mirrored.pixel(x, y);
 
             unsigned short syscolor = 0;
-            syscolor |= (qRed(color)&0xF8)   << 8;
-            syscolor |= (qGreen(color)&0xFC) << 3;
-            syscolor |= (qBlue(color)&0xF8)  >> 3;
-            stream << "0x" << QString::number(syscolor,16);
-            if(x!=mirrored.width()-1 || y!=mirrored.height()-1) stream << ", ";
+            syscolor |= (qRed(color) & 0xF8) << 8;
+            syscolor |= (qGreen(color) & 0xFC) << 3;
+            syscolor |= (qBlue(color) & 0xF8) >> 3;
+            stream << "0x" << QString::number(syscolor, 16);
+            if (x != mirrored.width() - 1 || y != mirrored.height() - 1)
+                stream << ", ";
         }
     }
     stream << endl << "};\n" << endl;
 
-    //creating the Picture structure
-    stream << "//the image structure {width, height, data}" << endl;
-    stream << "const Picture " << finfo.baseName() << " = {" << image.width() << ", " << image.height() << ", " << finfo.baseName() << "_data};";
+    // creating the Picture structure
+    stream << "// the image structure {width, height, data}" << endl;
+    stream << "const Picture " << finfo.baseName() << " = {" << image.width()
+           << ", " << image.height() << ", " << finfo.baseName() << "_data};";
 
     file.close();
 }
@@ -73,12 +78,12 @@ void exportImage(const QImage& image, const QString& filename)
 /**
  * @brief exportFont
  */
-void exportFont(QFont font, QString outFileName )
+void exportFont(QFont font, QString outFileName)
 {
     char c;
     unsigned value;
     int x, y, height, nb, width, wl;
-    char first=' ', last='z';
+    char first = ' ', last = 'z';
     bool prem;
 
     QFile file(outFileName);
@@ -88,24 +93,28 @@ void exportFont(QFont font, QString outFileName )
 
     QString fontName;
     QFontMetrics metric(font);
-    height=metric.height();
+    height = metric.height();
 
-    fontName=font.family().replace(" ","_");
-    //if (fontName.size()>10) fontName = fontName.mid(0,10);
-    if(font.pixelSize()!=-1)
+    fontName = font.family().replace(" ", "_");
+    // if (fontName.size()>10) fontName = fontName.mid(0,10);
+    if (font.pixelSize() != -1)
         fontName.append(QString::number(font.pixelSize()));
     else
         fontName.append(QString::number(font.pointSize()));
-    if(font.bold()) fontName.append("b");
-    if(font.underline()) fontName.append("u");
-    if(font.italic()) fontName.append("i");
+    if (font.bold())
+        fontName.append("b");
+    if (font.underline())
+        fontName.append("u");
+    if (font.italic())
+        fontName.append("i");
 
-    stream << QString("#ifndef _")+fontName+QString("_font_") << endl;
-    stream << QString("#define _")+fontName+QString("_font_") << endl << endl;
+    stream << QString("#ifndef _") + fontName + QString("_font_") << endl;
+    stream << QString("#define _") + fontName + QString("_font_") << endl
+           << endl;
     stream << QString("#include \"gui/font.h\"") << endl << endl;
 
-    QImage letter(50,50,QImage::Format_ARGB32);
-    QImage letters(2000,50,QImage::Format_ARGB32);
+    QImage letter(50, 50, QImage::Format_ARGB32);
+    QImage letters(2000, 50, QImage::Format_ARGB32);
     QPainter paint;
     QPainter paint2(&letters);
     font.setStyleStrategy(QFont::NoAntialias);
@@ -113,9 +122,9 @@ void exportFont(QFont font, QString outFileName )
     paint2.setBrush(Qt::white);
     paint2.drawRect(-1, -1, 2002, 52);
     wl = 0;
-    for(c=first; c<=last; ++c)
+    for (c = first; c <= last; ++c)
     {
-        if (c!='\\')
+        if (c != '\\')
             stream << "// " << c << endl;
         else
             stream << "// (antislash)" << endl;
@@ -126,68 +135,76 @@ void exportFont(QFont font, QString outFileName )
         paint.begin(&letter);
         paint.setBrush(Qt::white);
         paint.setFont(font);
-        paint.drawRect(QRect(-1,-1,52,52));
-        paint.drawText(QRect(0,0,50,50),Qt::AlignLeft|Qt::AlignTop,QString(c));
+        paint.drawRect(QRect(-1, -1, 52, 52));
+        paint.drawText(QRect(0, 0, 50, 50), Qt::AlignLeft | Qt::AlignTop,
+                       QString(c));
         paint.end();
 
-        width=metric.width(c);
-        paint2.drawText(QRect(wl,0,50,50),Qt::AlignLeft|Qt::AlignTop,QString(c));
-        wl+=width;
+        width = metric.width(c);
+        paint2.drawText(QRect(wl, 0, 50, 50), Qt::AlignLeft | Qt::AlignTop,
+                        QString(c));
+        wl += width;
         prem = true;
-        for(x=0; x<width; ++x)
+        for (x = 0; x < width; ++x)
         {
-            nb=0;
-            value=0;
-            for(y=0; y<height; ++y)
+            nb = 0;
+            value = 0;
+            for (y = 0; y < height; ++y)
             {
-                if((y%8)==0 && y!=0)
+                if ((y % 8) == 0 && y != 0)
                 {
-                    if(!prem)
+                    if (!prem)
                         stream << ", ";
                     prem = false;
                     stream << "0x";
-                    if(QString::number(value,16).size()<2)
+                    if (QString::number(value, 16).size() < 2)
                         stream << '0';
-                    stream << QString::number(value,16).toUpper();
-                    value=0;
+                    stream << QString::number(value, 16).toUpper();
+                    value = 0;
                 }
-                if(qGray(letter.pixel(x,y)) == 0)
+                if (qGray(letter.pixel(x, y)) == 0)
                 {
-                    value=value+(1<<(y%8));
+                    value = value + (1 << (y % 8));
                     nb++;
                 }
             }
-            if((y%8+1)!=0)
+            if ((y % 8 + 1) != 0)
             {
-                if(!prem)
+                if (!prem)
                     stream << ", ";
                 prem = false;
                 stream << "0x";
-                if(QString::number(value,16).size()<2)
+                if (QString::number(value, 16).size() < 2)
                     stream << '0';
-                stream << QString::number(value,16).toUpper();
-                value=0;
+                stream << QString::number(value, 16).toUpper();
+                value = 0;
             }
         }
         stream << "};" << endl;
-        stream << "const Letter " << fontName << "_letter_" << QString::number(c) << " = {" << QString::number(width) << ", " << fontName << "_data_" << QString::number(c) << "};" << endl;
+        stream << "const Letter " << fontName << "_letter_"
+               << QString::number(c) << " = {" << QString::number(width) << ", "
+               << fontName << "_data_" << QString::number(c) << "};" << endl;
     }
     stream << endl << "// Table of letter struct" << endl;
     QString declareTable = "const Letter* " + fontName + "_letters[] = {";
     stream << declareTable << endl;
-    for(c=first; c<=last; ++c)
+    for (c = first; c <= last; ++c)
     {
-        stream << QString(" ").repeated(declareTable.size()) << "&" << fontName << "_letter_" << QString::number(c) << ", " << endl;
+        stream << QString(" ").repeated(declareTable.size()) << "&" << fontName
+               << "_letter_" << QString::number(c) << ", " << endl;
     }
-    stream << QString(" ").repeated(declareTable.size()-1) << "};";
+    stream << QString(" ").repeated(declareTable.size() - 1) << "};";
     stream << endl << endl << "// Font structure" << endl;
-    stream << "const Font " << fontName << " = {" << QString::number(height) << ", " << QString::number(first) << ", " << QString::number(last) << ", " << fontName << "_letters" << "};";
+    stream << "const Font " << fontName << " = {" << QString::number(height)
+           << ", " << QString::number(first) << ", " << QString::number(last)
+           << ", " << fontName << "_letters"
+           << "};";
 
     stream << endl << "#endif";
 
     paint2.end();
     letters = letters.copy(0, 0, wl, height);
-    letters.save(outFileName+".png");
+    letters.save(outFileName + ".png");
 }
 
 int main(int argc, char *argv[])
@@ -202,9 +219,15 @@ int main(int argc, char *argv[])
     parser.addHelpOption();
     parser.addVersionOption();
 
-    QCommandLineOption inputOption(QStringList() << "i" << "input", "Input image file (jpg, png, bmp ...)", "image.jpg");
+    QCommandLineOption inputOption(QStringList() << "i"
+                                                 << "input",
+                                   "Input image file (jpg, png, bmp ...)",
+                                   "image.jpg");
     parser.addOption(inputOption);
-    QCommandLineOption outputOption(QStringList() << "o" << "output", "Write generated data into <file>.", "image.c");
+    QCommandLineOption outputOption(QStringList() << "o"
+                                                  << "output",
+                                    "Write generated data into <file>.",
+                                    "image.c");
     parser.addOption(outputOption);
 
     parser.process(app);
@@ -214,7 +237,7 @@ int main(int argc, char *argv[])
         out << family << endl;*/
 
     // check input
-    if(!parser.isSet(inputOption))
+    if (!parser.isSet(inputOption))
     {
         out << "No input file or police name specified." << endl;
         return 1;
@@ -228,25 +251,35 @@ int main(int argc, char *argv[])
 
     // image mode
     QStringList imageExts;
-    imageExts << "jpg" << "jpeg" << "png" << "bmp";
-    if(imageExts.contains(fileInfo.suffix(), Qt::CaseInsensitive))
+    imageExts << "jpg"
+              << "jpeg"
+              << "png"
+              << "bmp";
+    if (imageExts.contains(fileInfo.suffix(), Qt::CaseInsensitive))
     {
-        if(!QFile::exists(fileInfo.absoluteFilePath()))
+        if (!QFile::exists(fileInfo.absoluteFilePath()))
         {
-            out << "File " << inputFile << " does not exist.";
+            out << "File " << inputFile << " does not exist." << endl;
             return 1;
         }
         QImage image(fileInfo.absoluteFilePath());
+        if (image.size().width() == 0 || image.size().height() == 0)
+        {
+            out << "Invalid image file format." << endl;
+            return 1;
+        }
         exportImage(image, outputFile);
+        out << "Image " << inputFile << " " << image.size().width() << " x "
+            << image.size().height() << " px" << endl;
 
         return 0;
     }
 
     // font mode
     QRegExp regPolice("([a-zA-Z_]+)([0-9]+)([bui]*)");
-    if(regPolice.indexIn(inputFile) == 0)
+    if (regPolice.indexIn(inputFile) == 0)
     {
-        QString fontName = regPolice.cap(1).replace('_'," ");
+        QString fontName = regPolice.cap(1).replace('_', " ");
         int fontSize = regPolice.cap(2).toUInt();
         QString fontStyle = regPolice.cap(3);
 
