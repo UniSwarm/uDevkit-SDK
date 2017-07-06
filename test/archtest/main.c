a1d9fc7dd13d1fb242f01c4a34fa568c1eb9301d
sebcaux <sebastien.caux@robotips.fr>
2017-07-06 12:26:32 +0200

    [test] Added a simple project to test various driver

---
+++ test/archtest/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "modules.h"
+#include "board.h"
+#include <archi.h>
+
+int main(void)
+{
+    arch_init();
+
+    while(1);
+
+    return 0;
+}
