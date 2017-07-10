d7f3948a8ab17ea0b1654f665b9e1b717e2e823a
sebcaux <sebastien.caux@robotips.fr>
2017-07-10 15:48:19 +0100

    [pic32mk] Added 32MK arch init

---
+++ support/archi/pic32mk/pic32mk.c
@@ -0,0 +1,29 @@
+/**
+ * @file pic32mk.c
+ * @author Sebastien CAUX (sebcaux)
+ * @copyright Robotips 2017
+ *
+ * @date July 10, 2017, 16:15 PM
+ *
+ * @brief Architecture low level definitions for PIC32MK
+ */
+
+#include <archi.h>
+
+void archi_init()
+{
+    // enable multiple vector interrupts
+    INTCONSET = _INTCON_MVEC_MASK;
+    PRISS = 0x76543210;
+    enable_interrupt();
+}
+
+void archi_prefetch_enable()
+{
+    _mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, (_mfc0(_CP0_CONFIG, _CP0_CONFIG_SELECT) | 0x3));
+}
+
+void archi_prefetch_disable()
+{
+    _mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, ((_mfc0(_CP0_CONFIG, _CP0_CONFIG_SELECT) & 0xFFFFFFFC) | 0x2));
+}
--- support/archi/pic32mk/pic32mk.mk
+++ support/archi/pic32mk/pic32mk.mk
@@ -1,2 +1,5 @@
 
+vpath %.c $(RTPROG)/support/archi/pic32mk/
+ARCHI_SRC += pic32mk.c
+
 include $(RTPROG)/support/archi/pic32/pic32.mk
