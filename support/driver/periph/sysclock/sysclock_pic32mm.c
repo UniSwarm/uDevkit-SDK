69be20d6933ba0b794f9d4829ff4e70b4266a4c5
sebcaux <sebastien.caux@robotips.fr>
2017-07-11 20:00:41 +0200

    [sysclock:pic32mm] Added base implementation for PIC32MM (PLL with DCO)

---
+++ support/driver/periph/sysclock/sysclock_pic32mm.c
@@ -0,0 +1,148 @@
+/**
+ * @file sysclock_pic32mm.c
+ * @author Sebastien CAUX (sebcaux)
+ * @copyright Robotips 2017
+ *
+ * @date July 11, 2017, 18:30 PM
+ *
+ * @brief System clock support for rtprog for PIC32MM family
+ *
+ * Implementation based on Microchip document DS60001329B :
+ *  http://ww1.microchip.com/downloads/en/DeviceDoc/60001329b.pdf
+ */
+
+#include "sysclock.h"
+
+#include <archi.h>
+#include "board.h"
+
+uint32_t sysclock_sysfreq = 200000000;
+
+/**
+ * @brief Gets the actual frequency on a particular peripherical bus clock
+ * @param busClock id of the bus clock
+ * @return bus frequency in Hz
+ */
+uint32_t sysclock_getPeriphClock(SYSCLOCK_CLOCK busClock)
+{
+    if (busClock == SYSCLOCK_CLOCK_SYSCLK || busClock == SYSCLOCK_CLOCK_PBCLK)
+        return sysclock_sysfreq;
+    return 1;
+}
+
+/**
+ * @brief Change the divisor of the busClock given as argument. This can take up to 60
+ * CPU cycles.
+ * @param busClock id of the bus clock (SYSCLOCK_CLOCK_REFCLK)
+ * @param div divisor to set
+ * @return 0 if ok, -1 in case of error
+ */
+int sysclock_setPeriphClockDiv(SYSCLOCK_CLOCK busClock, uint8_t div)
+{
+    // checks
+    // TODO
+    if (busClock != SYSCLOCK_CLOCK_REFCLK)  // bad index
+        return -1;
+
+    // TODO implement me
+
+    return 0;
+}
+
+/**
+ * @brief Return the actual clock source for system clock
+ * @return SYSCLOCK_SOURCE enum corresponding to actual clock source
+ */
+SYSCLOCK_SOURCE sysclock_source()
+{
+    SYSCLOCK_SOURCE source = (SYSCLOCK_SOURCE)OSCCONbits.COSC;
+    return source;
+}
+
+/**
+ * @brief Switch the source clock of sysclock to another one and wait for the change effective
+ * @param source id to switch to
+ * @return 0 if ok, -1 in case of error
+ */
+int sysclock_switchSourceTo(SYSCLOCK_SOURCE source)
+{
+    if (OSCCONbits.CLKLOCK == 1)
+        return -1; // Clocks and PLL are locked, source cannot be changed
+
+#ifdef SYSCLOCK_SRC_BFRC
+    if (source == SYSCLOCK_SRC_BFRC)
+        return -2; // cannot switch to backup FRC
+#endif
+
+    // disable interrupts
+    disable_interrupt();
+
+    // unlock clock config (OSCCON is write protected)
+    unlockClockConfig();
+
+    // select the new source
+    OSCCONbits.NOSC = source;
+
+    // trigger change
+    OSCCONSET = _OSCCON_OSWEN_MASK;
+    nop();
+    nop();
+
+    // relock clock config
+    lockClockConfig();
+
+    while (OSCCONbits.OSWEN == 1)
+        nop();
+
+    // enable interrupts
+    enable_interrupt();
+
+    if (sysclock_source() != source)
+        return -3; // Error when switch clock source
+
+    return 0;
+}
+
+/**
+ * @brief Sets the system clock of the CPU, the system clock may be different of CPU
+ * clock and peripherical clock
+ * @param fosc desirate system frequency in Hz
+ * @return 0 if ok, -1 in case of error
+ */
+int sysclock_setClock(uint32_t fosc)
+{
+    //return sysclock_setClockWPLL(fosc);
+    sysclock_sysfreq = fosc;
+    return 0;
+}
+
+/**
+ * @brief Internal function to set clock with PLL from XTAL or FRC
+ * @param fosc desirate system frequency in Hz
+ * @param src input source clock of PLL (SYSCLOCK_SRC_FRC or SYSCLOCK_SRC_POSC)
+ * @return 0 if ok, -1 in case of error
+ */
+int sysclock_setPLLClock(uint32_t fosc, uint8_t src)
+{
+    // TODO implement me
+    return 0;
+}
+
+/**
+ * @brief Gets system frequency in Hz
+ * @return system frequency in Hz
+ */
+uint32_t sysclock_getClock()
+{
+    return sysclock_sysfreq;
+}
+
+/**
+ * @brief Gets CPU clock frequency in Hz
+ * @return cpu frequency in Hz
+ */
+uint32_t sysclock_getCPUClock()
+{
+    return sysclock_sysfreq;
+}
+
---
+++ support/driver/periph/sysclock/sysclock_pic32mm.h
@@ -0,0 +1,100 @@
+/**
+ * @file sysclock_pic32mm.h
+ * @author Sebastien CAUX (sebcaux)
+ * @copyright Robotips 2017
+ *
+ * @date July 11, 2017, 18:30 PM
+ *
+ * @brief System clock support for rtprog for PIC32MM family
+ *
+ * Implementation based on Microchip document DS60001329B :
+ *  http://ww1.microchip.com/downloads/en/DeviceDoc/60001329b.pdf
+ */
+
+#include "sysclock.h"
+
+#ifndef SYSCLOCK_PIC32MM_H
+#define SYSCLOCK_PIC32MM_H
+
+// bus clock
+typedef enum {
+    SYSCLOCK_CLOCK_SYSCLK = 0x0,    ///< main sysclock
+    SYSCLOCK_CLOCK_PBCLK,           ///< periphericals bus
+    SYSCLOCK_CLOCK_REFCLK,          ///< reference clock module
+} SYSCLOCK_CLOCK;
+uint32_t sysclock_getPeriphClock(SYSCLOCK_CLOCK busClock);
+int sysclock_setPeriphClockDiv(SYSCLOCK_CLOCK busClock, uint8_t div);
+
+// periph clock assoc
+#define SYSCLOCK_CLOCK_CPU    SYSCLOCK_CLOCK_SYSCLK  ///< CPU clock bus
+#define SYSCLOCK_CLOCK_USB    SYSCLOCK_CLOCK_USBCLK  ///< USB clock bus
+#define SYSCLOCK_CLOCK_CAN    SYSCLOCK_CLOCK_PBCLK  ///< CAN clock bus mapped to periphericals bus 5
+#define SYSCLOCK_CLOCK_PMP    SYSCLOCK_CLOCK_PBCLK  ///< PMP clock bus mapped to periphericals bus 2
+#define SYSCLOCK_CLOCK_I2C    SYSCLOCK_CLOCK_PBCLK  ///< I2C clock bus mapped to periphericals bus 2
+#define SYSCLOCK_CLOCK_UART   SYSCLOCK_CLOCK_PBCLK  ///< UART clock bus mapped to periphericals bus 2
+#define SYSCLOCK_CLOCK_SPI    SYSCLOCK_CLOCK_PBCLK  ///< SPI clock bus mapped to periphericals bus 2
+#define SYSCLOCK_CLOCK_TIMER  SYSCLOCK_CLOCK_PBCLK  ///< TIMER clock bus mapped to periphericals bus 3
+#define SYSCLOCK_CLOCK_OC     SYSCLOCK_CLOCK_PBCLK  ///< OC clock bus mapped to periphericals bus 3
+#define SYSCLOCK_CLOCK_IC     SYSCLOCK_CLOCK_PBCLK  ///< IC clock bus mapped to periphericals bus 3
+
+// clock source
+typedef enum {
+#if defined(ARCHI_pic32mzda) || defined(ARCHI_pic32mzec) || defined(ARCHI_pic32mzef)
+    SYSCLOCK_SRC_FRC2  = 0b111, ///< sysclock from Fast RC Oscillator (FRC) divided by FRCDIV
+    SYSCLOCK_SRC_BFRC  = 0b110, ///< backup FRC, cannot be chose by user
+#endif
+    SYSCLOCK_SRC_LPRC  = 0b101, ///< Low-Power RC (LPRC) Oscillator
+#if defined(ARCHI_pic32mk)
+    SYSCLOCK_SRC_UPLL  = 0b011, ///< USB PLL (UPLL)
+#endif
+    SYSCLOCK_SRC_SOSC  = 0b100, ///< SOSC
+    SYSCLOCK_SRC_POSC  = 0b010, ///< Primary Oscillator (POSC) HS and EC
+    SYSCLOCK_SRC_SPLL  = 0b001, ///< System PLL (SPLL)
+    SYSCLOCK_SRC_FRC   = 0b000  ///< same as SYSCLOCK_SRC_FRC
+} SYSCLOCK_SOURCE;
+SYSCLOCK_SOURCE sysclock_source();
+int sysclock_switchSourceTo(SYSCLOCK_SOURCE source);
+
+#if defined(ARCHI_pic32mzda) || defined(ARCHI_pic32mzec) || defined(ARCHI_pic32mzef)
+ #define SYSCLOCK_SRC_FRC2 SYSCLOCK_SRC_FRC2
+ #define SYSCLOCK_SRC_BFRC SYSCLOCK_SRC_BFRC
+#elif defined(ARCHI_pic32mk)
+ #define SYSCLOCK_SRC_UPLL SYSCLOCK_SRC_UPLL
+#endif
+
+/*  main PLL
+ *        __         _____         _____         _____
+ * FRC-->|  \  Fin  |     | Fplli |     | Fvco  |     | Fpllo
+ *       |   |----->| /Nx |------>| x M |------>| /Ny |------>
+ * Posc->|__/       |_____|       |_____|       |_____|
+ *     PLLICLK
+ *
+ *  SYSCLOCK_FPLLI_MIN > Fplli > SYSCLOCK_FPLLI_MAX
+ *  SYSCLOCK_FVCO_MIN  > Fvco  > SYSCLOCK_FVCO_MAX
+ *  SYSCLOCK_FPLLO_MIN > Fpllo > SYSCLOCK_FPLLO_MAX
+ *  SYSCLOCK_FSYS_MIN  > Fsys  > SYSCLOCK_FSYS_MAX
+ *  Nx = {1-8} runtime editable
+ *  M = {1-128} runtime editable
+ *  Ny = {2, 4, 8, 16, 32} runtime editable
+ */
+
+
+// pre-diviser N
+#define SYSCLOCK_NX_MIN 1
+#define SYSCLOCK_NX_MAX 8
+
+#define SYSCLOCK_FPLLI_MIN   5000000    //   5 MHz
+#define SYSCLOCK_FPLLI_MAX  64000000    //  64 MHz
+#define SYSCLOCK_FVCO_MIN  350000000    // 350 MHz
+#define SYSCLOCK_FVCO_MAX  700000000    // 700 MHz
+#define SYSCLOCK_FPLLO_MIN  10000000    //  10 MHz
+#define SYSCLOCK_FPLLO_MAX 120000000    // 120 MHz
+
+#define SYSCLOCK_FSYS_MIN 8000000
+#define SYSCLOCK_FSYS_MAX 120000000
+
+#define SYSCLOCK_FOSC_MAX 120000000
+
+int sysclock_setPLLClock(uint32_t freq, uint8_t src);
+
+#endif // SYSCLOCK_PIC32MM_H
