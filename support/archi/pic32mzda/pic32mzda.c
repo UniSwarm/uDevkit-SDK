b766ab2c84559035583f6440ee40a89a72f00fc1
sebcaux <sebastien.caux@robotips.fr>
2017-07-11 19:47:16 +0200

    [archi:pic32mzda] Added archi_init base implementation, need to be checked

---
+++ support/archi/pic32mzda/pic32mzda.c
@@ -0,0 +1,24 @@
+/**
+ * @file pic32mzda.c
+ * @author Sebastien CAUX (sebcaux)
+ * @copyright Robotips 2017
+ *
+ * @date July 11, 2017, 19:35 PM
+ *
+ * @brief Architecture low level definitions for PIC32MZDA
+ */
+
+#include <archi.h>
+
+void archi_init()
+{
+    // Prefetch-cache: Enable prefetch for PFM (any PFM instructions or data)
+    PRECONbits.PREFEN = 3;
+    // Flash PM Wait States: MZ Flash runs at 2 wait states @ 200 MHz
+    PRECONbits.PFMWS = 2;
+
+    // enable multiple vector interrupts
+    INTCONSET = _INTCON_MVEC_MASK;
+    PRISS = 0x76543210;
+    enable_interrupt();
+}
--- support/archi/pic32mzda/pic32mzda.mk
+++ support/archi/pic32mzda/pic32mzda.mk
@@ -1,2 +1,5 @@
 
+vpath %.c $(RTPROG)/support/archi/pic32mzda/
+ARCHI_SRC += pic32mzda.c
+
 include $(RTPROG)/support/archi/pic32mz/pic32mz.mk
