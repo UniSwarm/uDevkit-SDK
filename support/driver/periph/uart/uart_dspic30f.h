cfac837a8d23c3909519093ec5e4ad155c8505b9
sebcaux <sebastien.caux@robotips.fr>
2017-08-04 18:07:46 +0200

    [uart:dspic30f] Fixed device count

--- support/driver/periph/uart/uart_dspic30f.h
+++ support/driver/periph/uart/uart_dspic30f.h
@@ -15,19 +15,21 @@
 #define UART_DSPIC30F_H
 
 // calculate UART count on the device
-#if defined (DEVICE_30F1010) || defined (DEVICE_30F2010) || defined (DEVICE_30F2011) \
- || defined (DEVICE_30F2012) || defined (DEVICE_30F2020) || defined (DEVICE_30F2023) \
- || defined (DEVICE_30F3010) || defined (DEVICE_30F3012) || defined (DEVICE_30F4012) \
- || defined (DEVICE_30F5015) || defined (DEVICE_30F5016)
-  #define UART_COUNT 1
-#elif defined (DEVICE_30F3011) || defined (DEVICE_30F3013) || defined (DEVICE_30F3014) \
- || defined (DEVICE_30F4011) || defined (DEVICE_30F4013) || defined (DEVICE_30F5011) \
- || defined (DEVICE_30F5013) || defined (DEVICE_30F6010A) || defined (DEVICE_30F6011A) \
- || defined (DEVICE_30F6012A) || defined (DEVICE_30F6013A) || defined (DEVICE_30F6014A) \
- || defined (DEVICE_30F6015)
-  #define UART_COUNT 2
+
+#if defined(DEVICE_30F1010) || defined(DEVICE_30F2010) || defined(DEVICE_30F2011) \
+ || defined(DEVICE_30F2012) || defined(DEVICE_30F2020) || defined(DEVICE_30F2023) \
+ || defined(DEVICE_30F3010) || defined(DEVICE_30F3012) || defined(DEVICE_30F4012) \
+ || defined(DEVICE_30F5015) || defined(DEVICE_30F5016)
+ #define UART_COUNT 1
+#elif defined(DEVICE_30F3011) || defined(DEVICE_30F3013) || defined(DEVICE_30F3014) \
+ || defined(DEVICE_30F4011) || defined(DEVICE_30F4013) || defined(DEVICE_30F5011) \
+ || defined(DEVICE_30F5013) || defined(DEVICE_30F6010) || defined(DEVICE_30F6010A) \
+ || defined(DEVICE_30F6011) || defined(DEVICE_30F6011A) || defined(DEVICE_30F6012) \
+ || defined(DEVICE_30F6012A) || defined(DEVICE_30F6013) || defined(DEVICE_30F6013A) \
+ || defined(DEVICE_30F6014) || defined(DEVICE_30F6014A) || defined(DEVICE_30F6015)
+ #define UART_COUNT 2
 #else
-  #define UART_COUNT 0
+ #define UART_COUNT 0
 #endif
 
 #endif // UART_DSPIC30F_H
