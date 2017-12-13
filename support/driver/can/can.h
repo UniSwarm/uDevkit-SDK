93df3cf912bcfde46afa3784183189677ad372c8
sebcaux <sebastien.caux@robotips.fr>
2017-12-13 16:18:54 +0100

    [can:pic32] Implemented bit config

--- support/driver/can/can.h
+++ support/driver/can/can.h
@@ -13,17 +13,17 @@
 
 #include <driver/device.h>
 
-// ====== device assignation ======
+// ====== bus assignation ======
 #define can(d) MKDEV(DEV_CLASS_CAN, (d)-1)
 rt_dev_t can_getFreeDevice();
 int can_open(rt_dev_t device);
 int can_close(rt_dev_t device);
 
-// ==== device enable/disable =====
+// ==== bus enable/disable =====
 int can_enable(rt_dev_t device);
 int can_disable(rt_dev_t device);
 
-// ======== device settings =======
+// ======== bus settings =======
 typedef enum {
     CAN_VERSION_1 = 0x0,        ///< CAN version 1 with standard 11 bits identifier
     CAN_VERSION_2A,             ///< CAN version 2A, extended identifier are error
@@ -31,19 +31,26 @@ typedef enum {
     CAN_VERSION_2B_ACTIVE       ///< CAN version 2B, work with extended 29 bits
                                 /// identifier and standard 11 bits identifier
 } CAN_VERSION;
-
-int can_setConfig(rt_dev_t device, uint8_t mode, CAN_VERSION canVersion, uint32_t baudSpeed);
-uint32_t can_baudSpeed(rt_dev_t device);
-uint32_t can_effectiveBaudSpeed(rt_dev_t device);
+int can_setConfig(rt_dev_t device, uint8_t mode, CAN_VERSION canVersion);
 uint8_t can_mode(rt_dev_t device);
 uint8_t can_canVersion(rt_dev_t device);
+
+int can_setBitTiming(rt_dev_t device, uint32_t bitRate, uint8_t propagSeg, uint8_t s1Seg, uint8_t s2Seg);
+uint32_t can_bitRate(rt_dev_t device);
+uint32_t can_effectiveBitRate(rt_dev_t device);
+uint8_t can_propagSeg(rt_dev_t device);
+uint8_t can_s1Seg(rt_dev_t device);
+uint8_t can_s2Seg(rt_dev_t device);
+
+// ===== mailbox interface =====
+// TODO
 
 #if defined(ARCHI_pic24ep) || defined(ARCHI_pic24f) || defined(ARCHI_pic24fj) \
  || defined(ARCHI_pic24hj) || defined(ARCHI_dspic33fj) || defined(ARCHI_dspic33ep) \
  || defined(ARCHI_dspic33ev)
-  #include "can_pic24_dspic33.h"
+ #include "can_pic24_dspic33.h"
 #elif defined(ARCHI_dspic30f)
-  #include "can_dspic30f.h"
+ #include "can_dspic30f.h"
 #elif defined(ARCHI_pic32mx) || defined(ARCHI_pic32mk) || defined(ARCHI_pic32mzec) \
  || defined(ARCHI_pic32mzef) || defined(ARCHI_pic32mzda)
  #include "can_pic32.h"
--- support/driver/can/can_pic32.c
+++ support/driver/can/can_pic32.c
@@ -36,32 +36,35 @@ typedef struct {
 
 struct can_dev
 {
-    uint32_t baudSpeed;
     uint8_t mode;
     CAN_VERSION canVersion;
+    uint32_t bitRate;
+    uint8_t propagSeg;
+    uint8_t s1Seg;
+    uint8_t s2Seg;
     can_status flags;
 };
 
 struct can_dev cans[] = {
     {
-        .baudSpeed = 0,
+        .bitRate = 0,
         .flags = {{.val = CAN_FLAG_UNUSED}}
     },
 #if CAN_COUNT>=2
     {
-        .baudSpeed = 0,
+        .bitRate = 0,
         .flags = {{.val = CAN_FLAG_UNUSED}}
     },
 #endif
 #if CAN_COUNT>=3
     {
-        .baudSpeed = 0,
+        .bitRate = 0,
         .flags = {{.val = CAN_FLAG_UNUSED}}
     },
 #endif
 #if CAN_COUNT>=4
     {
-        .baudSpeed = 0,
+        .bitRate = 0,
         .flags = {{.val = CAN_FLAG_UNUSED}}
     },
 #endif
@@ -195,16 +198,14 @@ int can_disable(rt_dev_t device)
 }
 
 /**
- * @brief Sets configuration (speed of receive and transmit, can version and mode) of the specified can device
+ * @brief Sets configuration (can version and mode) of the specified can device
  * @param device can device number
- * @param baudSpeed speed of receive and transmit in bauds (bits / s)
+ * @param mode CAN mode of operation
+ * @param canVersion version of CAN bus
  * @return 0 if ok, -1 in case of error
  */
-int can_setConfig(rt_dev_t device, uint8_t mode, CAN_VERSION canVersion, uint32_t baudSpeed)
+int can_setConfig(rt_dev_t device, uint8_t mode, CAN_VERSION canVersion)
 {
-    uint32_t systemClockPeriph;
-    uint16_t uBrg;
-    uint8_t hs = 0;
     uint8_t enabled = 0;
 
     // check parameters
@@ -219,13 +220,115 @@ int can_setConfig(rt_dev_t device, uint8_t mode, CAN_VERSION canVersion, uint32_
 
     return 0;
 }
+uint8_t can_mode(rt_dev_t device);
+uint8_t can_canVersion(rt_dev_t device);
+
+int can_setBitTiming(rt_dev_t device, uint32_t bitRate, uint8_t propagSeg, uint8_t s1Seg, uint8_t s2Seg)
+{
+    uint8_t can = MINOR(device);
+    uint8_t bitRateDiv;
+    uint8_t quantum;
+    if (can >= CAN_COUNT)
+        return 0;
+
+    if (propagSeg > 8 || s1Seg > 8 || s2Seg > 8)
+        return -1;
+    quantum = propagSeg + s1Seg + s2Seg + 1;
+    if (quantum < 8 || quantum > 25)
+        return -1;
+
+    // CAN Bit Timing (8-25 Tq) segments computation
+    // | Sync | Propag seg | Phase seg 1 | Phase seg 2 |
+    // | 1 Tq |   1-8 Tq   |   1-8 Tq    |   1-8 Tq    |
+    //                              sample point
+    cans[can].bitRate = bitRate;
+    cans[can].propagSeg = propagSeg;
+    cans[can].s1Seg = s1Seg;
+    cans[can].s2Seg = s2Seg;
+
+    bitRateDiv = sysclock_periphFreq(SYSCLOCK_CLOCK_CAN) / (quantum * 2);
+
+    switch (can)
+    {
+    case 0:
+        C1CONbits.REQOP = 4;
+        while (C1CONbits.OPMOD != 4);
+
+        C1CFGbits.SJW    = 1;          // Synchronization Jump Width (1-4)
+        C1CFGbits.PRSEG  = propagSeg;  // Propagation Time Segment (1-8)
+        C1CFGbits.SEG1PH = s1Seg;      // Phase Buffer Segment 1 (1-8)
+        C1CFGbits.SEG2PHTS = 1;        // Phase Buffer Segment 2 is freely programmable
+        C1CFGbits.SEG2PH = s2Seg;      // Phase Buffer Segment 2 (1-8) SEG2PH ≤ SEG1PH
+        C1CFGbits.BRP    = bitRateDiv; // bit rate divisor (1-64) * 2
+        break;
+#if CAN_COUNT>=2
+    case 1:
+        C2CONbits.REQOP = 4;
+        while (C2CONbits.OPMOD != 4);
+
+        C2CFGbits.SJW    = 1;          // Synchronization Jump Width (1-4)
+        C2CFGbits.PRSEG  = propagSeg;  // Propagation Time Segment (1-8)
+        C2CFGbits.SEG1PH = s1Seg;      // Phase Buffer Segment 1 (1-8)
+        C2CFGbits.SEG2PHTS = 1;        // Phase Buffer Segment 2 is freely programmable
+        C2CFGbits.SEG2PH = s2Seg;      // Phase Buffer Segment 2 (1-8) SEG2PH ≤ SEG1PH
+        C2CFGbits.BRP    = bitRateDiv; // bit rate divisor (1-64) * 2
+        break;
+#endif
+#if CAN_COUNT>=3
+    case 2:
+        C3CONbits.REQOP = 4;
+        while (C3CONbits.OPMOD != 4);
+
+        C3CFGbits.SJW    = 1;          // Synchronization Jump Width (1-4)
+        C3CFGbits.PRSEG  = propagSeg;  // Propagation Time Segment (1-8)
+        C3CFGbits.SEG1PH = s1Seg;      // Phase Buffer Segment 1 (1-8)
+        C3CFGbits.SEG2PHTS = 1;        // Phase Buffer Segment 2 is freely programmable
+        C3CFGbits.SEG2PH = s2Seg;      // Phase Buffer Segment 2 (1-8) SEG2PH ≤ SEG1PH
+        C3CFGbits.BRP    = bitRateDiv; // bit rate divisor (1-64) * 2
+        break;
+#endif
+#if CAN_COUNT>=4
+    case 3:
+        C4CONbits.REQOP = 4;
+        while (C4CONbits.OPMOD != 4);
+
+        C4CFGbits.SJW    = 1;          // Synchronization Jump Width (1-4)
+        C4CFGbits.PRSEG  = propagSeg;  // Propagation Time Segment (1-8)
+        C4CFGbits.SEG1PH = s1Seg;      // Phase Buffer Segment 1 (1-8)
+        C4CFGbits.SEG2PHTS = 1;        // Phase Buffer Segment 2 is freely programmable
+        C4CFGbits.SEG2PH = s2Seg;      // Phase Buffer Segment 2 (1-8) SEG2PH ≤ SEG1PH
+        C4CFGbits.BRP    = bitRateDiv; // bit rate divisor (1-64) * 2
+        break;
+#endif
+    }
+
+    return 0;
+}
 
 /**
- * @brief Gets the true baud speed of the specified can device
- * @param device can device number
- * @return speed of receive and transmit in bauds (bits / s)
+ * @brief Returns the curent bit rate in bits/s
+ * @param device CAN device number
+ * @return bit rate in bits/s if ok, 0 in case of error
  */
-uint32_t can_baudSpeed(rt_dev_t device)
+uint32_t can_bitRate(rt_dev_t device)
+{
+#if CAN_COUNT>=1
+    uint8_t can = MINOR(device);
+    if (can >= CAN_COUNT)
+        return 0;
+
+    return cans[can].bitRate;
+#else
+    return 0;
+#endif
+}
+
+/**
+ * @brief Gets the effective bit rate in hertz
+ * @param device CAN device number
+ * @return speed of receive and transmit in bits/s, 0 in case of error
+ */
+uint32_t can_effectiveBitRate(rt_dev_t device)
 {
     uint32_t baudSpeed;
     uint16_t uBrg;
@@ -239,16 +342,41 @@ uint32_t can_baudSpeed(rt_dev_t device)
     return baudSpeed;
 }
 
-/**
- * @brief Gets the effective baud speed of the specified can device
- * @param device can device number
- * @return speed of receive and transmit in bauds (bits / s)
- */
-uint32_t can_effectiveBaudSpeed(rt_dev_t device)
+uint8_t can_propagSeg(rt_dev_t device)
 {
+#if CAN_COUNT>=1
     uint8_t can = MINOR(device);
     if (can >= CAN_COUNT)
         return 0;
 
-    return cans[can].baudSpeed;
+    return cans[can].propagSeg;
+#else
+    return 0;
+#endif
+}
+
+uint8_t can_s1Seg(rt_dev_t device)
+{
+#if CAN_COUNT>=1
+    uint8_t can = MINOR(device);
+    if (can >= CAN_COUNT)
+        return 0;
+
+    return cans[can].s1Seg;
+#else
+    return 0;
+#endif
+}
+
+uint8_t can_s2Seg(rt_dev_t device)
+{
+#if CAN_COUNT>=1
+    uint8_t can = MINOR(device);
+    if (can >= CAN_COUNT)
+        return 0;
+
+    return cans[can].s2Seg;
+#else
+    return 0;
+#endif
 }
