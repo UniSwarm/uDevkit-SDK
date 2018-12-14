d082362324df7c468a7091545b4da2179cef6037
sebcaux <sebastien.caux@robotips.fr>
2018-12-14 11:31:28 +0100

    [adc:dspic33_sar12] Added base implementation

--- support/driver/adc/adc.h
+++ support/driver/adc/adc.h
@@ -2,6 +2,7 @@
  * @file adc.h
  * @author Sebastien CAUX (sebcaux)
  * @copyright Robotips 2016
+ * @copyright UniSwarm 2017-2018
  *
  * @date April 19, 2016, 10:56 AM
  *
@@ -20,7 +21,18 @@ int adc_init();
 int16_t adc_getValue(uint8_t channel);
 //int adc_setSamplingPeriod(uint16_t priodMs);
 
-#if defined(ARCHI_pic24ep) || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev)
+#if defined(DEVICE_33EP128GS702) || defined(DEVICE_33EP128GS704) || defined(DEVICE_33EP128GS705) \
+ || defined(DEVICE_33EP128GS706) || defined(DEVICE_33EP128GS708) || defined(DEVICE_33EP128GS804) \
+ || defined(DEVICE_33EP128GS805) || defined(DEVICE_33EP128GS806) || defined(DEVICE_33EP128GS808) \
+ || defined(DEVICE_33EP16GS202) || defined(DEVICE_33EP16GS502) || defined(DEVICE_33EP16GS504) \
+ || defined(DEVICE_33EP16GS505) || defined(DEVICE_33EP16GS506) || defined(DEVICE_33EP32GS202) \
+ || defined(DEVICE_33EP32GS502) || defined(DEVICE_33EP32GS504) || defined(DEVICE_33EP32GS505) \
+ || defined(DEVICE_33EP32GS506) || defined(DEVICE_33EP64GS502) || defined(DEVICE_33EP64GS504) \
+ || defined(DEVICE_33EP64GS505) || defined(DEVICE_33EP64GS506) || defined(DEVICE_33EP64GS708) \
+ || defined(DEVICE_33EP64GS804) || defined(DEVICE_33EP64GS805) || defined(DEVICE_33EP64GS806) \
+ || defined(DEVICE_33EP64GS808) || defined(ARCHI_dspic33ch) || defined(ARCHI_dspic33ck)
+ #include "adc_dspic33_sar12.h"
+#elif defined(ARCHI_pic24ep) || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev)
  #include "adc_pic24e_dspic33e.h"
 #elif defined(ARCHI_pic32mzec) || defined(ARCHI_pic32mzef)
  #include "adc_pic32mz.h"
--- support/driver/adc/adc.mk
+++ support/driver/adc/adc.mk
@@ -6,11 +6,24 @@ vpath %.h $(DRIVERPATH)
 
 HEADER += adc.h
 
-ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ep dspic33fj dspic33ev))
- ARCHI_SRC += adc_pic24e_dspic33e.c
- HEADER += adc_pic24e_dspic33e.h
+ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ch dspic33ck))
+ ARCHI_SRC += adc_dspic33_sar12.c
+ HEADER += adc_dspic33_sar12.h
 endif
-ifeq ($(ARCHI),$(filter $(ARCHI),pic32mm pic32mzda pic32mzec pic32mzef))
+ifeq ($(ARCHI), dspic33ep dspic33ev)
+ ifeq ($(DEVICE),$(filter $(DEVICE),33EP128GS702 33EP128GS704 33EP128GS705 \
+   33EP128GS706 33EP128GS708 33EP128GS804 33EP128GS805 33EP128GS806 33EP128GS808 \
+   33EP16GS202 33EP16GS502 33EP16GS504 33EP16GS505 33EP16GS506 33EP32GS202 \
+   33EP32GS502 33EP32GS504 33EP32GS505 33EP32GS506 33EP64GS502 33EP64GS504 \
+   33EP64GS505 33EP64GS506 33EP64GS708 33EP64GS804 33EP64GS805 33EP64GS806 33EP64GS808))
+  ARCHI_SRC += adc_dspic33_sar12.c
+  HEADER += adc_dspic33_sar12.h
+ else
+  ARCHI_SRC += adc_pic24e_dspic33e.c
+  HEADER += adc_pic24e_dspic33e.h
+ endif
+endif
+ifeq ($(ARCHI),$(filter $(ARCHI),pic32mzda pic32mzec pic32mzef))
  ARCHI_SRC += adc_pic32mz.c
  HEADER += adc_pic32mz.h
 endif
