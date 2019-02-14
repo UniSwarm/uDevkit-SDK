9643160d09499a2d24549efc34c0b59e9bc3ff2e
sebcaux <sebastien.caux@uniswarm.eu>
2019-02-14 14:54:49 +0100

    [oc] Fixed mk file syntax

--- support/driver/oc/oc.mk
+++ support/driver/oc/oc.mk
@@ -11,7 +11,7 @@ ifeq ($(ARCHI),$(filter $(ARCHI),pic24ep dspic33ep dspic33ev))
  HEADER += oc_pic24e_dspic33e.h
 endif
 ifeq ($(ARCHI), pic24fj)
- ifeq ($(DEVICE),$(filter $(DEVICE),24FJ16MC101 24FJ16MC102 24FJ32MC101 24FJ32MC102 24FJ32MC104)
+ ifeq ($(DEVICE),$(filter $(DEVICE),24FJ16MC101 24FJ16MC102 24FJ32MC101 24FJ32MC102 24FJ32MC104))
   ARCHI_SRC += oc_pic24f_dspic30f_dspic33f.c
   HEADER += oc_pic24f_dspic30f_dspic33f.h
  else
