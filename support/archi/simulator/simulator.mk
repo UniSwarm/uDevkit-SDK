80958389415f70018d3484e24a535c246ebbb4d7
sebcaux <sebastien.caux@robotips.fr>
2019-04-25 00:17:26 +0200

    [sim] Added intermediate rule to make exe only without run it

--- support/archi/simulator/simulator.mk
+++ support/archi/simulator/simulator.mk
@@ -33,21 +33,23 @@ $(SIM_OBJECTS) : $(CONFIG_HEADERS)
 # rule to build OBJECTS to OUT_PWD and give dependencies
 $(OUT_PWD)/%_simo.o : %.c
 	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
-	@printf "$(GREEN)CC %-36s => %s$(NORM)\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
+	@printf "$(COMPCOLOR)CC %-36s => %s$(NORM)\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
 	$(VERB)$(CC_SIM) $(CCFLAGS) -c  $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -o  $(OUT_PWD)/$(notdir $@)
 	@$(CC_SIM) $(CCFLAGS) -MM $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -MT $(OUT_PWD)/$(notdir $@) > $(OUT_PWD)/$*_sim.d
 $(OUT_PWD)/%_simcppo.o : %.cpp
 	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
-	@printf "$(GREEN)CPPC %-34s => %s$(NORM)\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
+	@printf "$(COMPCOLOR)CPPC %-34s => %s$(NORM)\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
 	$(VERB)$(CPPC_SIM) $(CCFLAGS) -c  $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -o  $(OUT_PWD)/$(notdir $@)
 	@$(CPPC_SIM) $(CCFLAGS) -MM $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -MT $(OUT_PWD)/$(notdir $@) > $(OUT_PWD)/$*_simcpp.d
 
 # rule to link SIM_OBJECTS to an elf in OUT_PWD
 $(OUT_PWD)/$(SIM_EXE) : $(SIM_OBJECTS)
-	@printf "$(GREEN)LD %-36s => %s$(NORM)\n" "*.o" $(OUT_PWD)/$(PROJECT).elf
+	@printf "$(COMPCOLOR)LD %-36s => %s$(NORM)\n" "*.o" $(OUT_PWD)/$(PROJECT).elf
 	$(VERB)$(CPPC_SIM) $(CCFLAGS) -o $(OUT_PWD)/$(SIM_EXE) $(addprefix $(OUT_PWD)/,$(notdir $(SIM_OBJECTS))) $(LIBS_SIM) $(DEFINES_SIM) -lm
 
-sim : $(OUT_PWD)/$(SIM_EXE) $(RTSIM_EXE)
+sim-exe: $(OUT_PWD)/$(SIM_EXE)
+
+sim : sim-exe $(RTSIM_EXE)
 	#killall rtsim || true
 	$(RTSIM_EXE) $(OUT_PWD)/$(SIM_EXE)
 
