085568c1b6c58dbb187d93cf6b8581de14f540d8
sebcaux <sebastien.caux@uniswarm.eu>
2020-09-19 10:59:58 +0200

    [sim] Reviewed sim build system with independent build dir and CROSS COMPILE management

--- support/archi/simulator/simulator.mk
+++ support/archi/simulator/simulator.mk
@@ -1,15 +1,27 @@
-
-CC_SIM ?= gcc
-CPPC_SIM ?= g++
-OUT_SIM_PWD ?= simbuild
+CC_SIM ?= $(CROSS_COMPILE)gcc
+CPPC_SIM ?= $(CROSS_COMPILE)g++
+ifeq ($(CROSS_COMPILE),)
+  OUT_SIM_PWD ?= build-sim
+else
+  OUT_SIM_PWD ?= build-sim-$(CROSS_COMPILE:-=)
+endif
 
 # SIM_EXE cmd
+LIBS_SIM += -lm
 ifeq ($(OS),Windows_NT)
-  SIM_EXE := $(PROJECT).exe
+  ifeq ($(CROSS_COMPILE),)
+    SIM_EXE := $(PROJECT)_sim.exe
+  else
+    SIM_EXE := $(PROJECT)_sim-$(CROSS_COMPILE:-=).exe
+  endif
   LIBS_SIM += -lws2_32
   UDKSIM_EXE := $(UDEVKIT)/bin/udk-sim.exe
 else
-  SIM_EXE := $(PROJECT)_sim
+  ifeq ($(CROSS_COMPILE),)
+    SIM_EXE := $(PROJECT)_sim
+  else
+    SIM_EXE := $(PROJECT)_sim-$(CROSS_COMPILE:-=)
+  endif
   LIBS_SIM += -pthread
   UDKSIM_EXE := $(UDEVKIT)/bin/udk-sim
 endif
@@ -27,33 +39,38 @@ vpath %.cpp $(SIMULATOR_PATH)
 SIM_SRC += simulator.cpp simulator_socket.c simulator_pthread.c
 HEADER += simulator.h simulator_socket.h simulator_pthread.h
 
+vpath %.h $(OUT_SIM_PWD)
+vpath %.c $(OUT_SIM_PWD)
+INCLUDEPATH += -I$(OUT_SIM_PWD)
+
 # simulator support
-SIM_OBJECTS := $(addprefix $(OUT_PWD)/, $(notdir $(SRC:.c=_simo.o) $(patsubst %.cpp,%_simcppo.o,$(SIM_SRC:.c=_simo.o))))
+SIM_OBJECTS := $(addprefix $(OUT_SIM_PWD)/, $(notdir $(SRC:.c=.o) $(patsubst %.cpp,%.o,$(SIM_SRC:.c=.o))))
 $(SIM_OBJECTS) : $(CONFIG_HEADERS)
 
-# rule to build OBJECTS to OUT_PWD and give dependencies
-$(OUT_PWD)/%_simo.o : %.c
-	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
-	@printf "$(COMPCOLOR)CC %-36s => %s$(NORM)\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
-	$(VERB)$(CC_SIM) $(CCFLAGS) -c  $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -o  $(OUT_PWD)/$(notdir $@)
-	@$(CC_SIM) $(CCFLAGS) -MM $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -MT $(OUT_PWD)/$(notdir $@) > $(OUT_PWD)/$*_sim.d
-$(OUT_PWD)/%_simcppo.o : %.cpp
-	@test -d $(OUT_PWD) || mkdir -p $(OUT_PWD)
-	@printf "$(COMPCOLOR)CPPC %-34s => %s$(NORM)\n" $(notdir $<) $(OUT_PWD)/$(notdir $@)
-	$(VERB)$(CPPC_SIM) $(CCFLAGS) -c  $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -o  $(OUT_PWD)/$(notdir $@)
-	@$(CPPC_SIM) $(CCFLAGS) -MM $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -MT $(OUT_PWD)/$(notdir $@) > $(OUT_PWD)/$*_simcpp.d
+# rule to build OBJECTS to OUT_SIM_PWD and give dependencies
+$(OUT_SIM_PWD)/%.o : %.c
+	@test -d $(OUT_SIM_PWD) || mkdir -p $(OUT_SIM_PWD)
+	@printf "$(COMPCOLOR)CC %-36s => %s$(NORM)\n" $(notdir $<) $(OUT_SIM_PWD)/$(notdir $@)
+	$(VERB)$(CC_SIM) $(CCFLAGS) -c  $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -o  $(OUT_SIM_PWD)/$(notdir $@)
+	@$(CC_SIM) $(CCFLAGS) -MM $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -MT $(OUT_SIM_PWD)/$(notdir $@) > $(OUT_SIM_PWD)/$*_sim.d
+$(OUT_SIM_PWD)/%.o : %.cpp
+	@test -d $(OUT_SIM_PWD) || mkdir -p $(OUT_SIM_PWD)
+	@printf "$(COMPCOLOR)CPPC %-34s => %s$(NORM)\n" $(notdir $<) $(OUT_SIM_PWD)/$(notdir $@)
+	$(VERB)$(CPPC_SIM) $(CCFLAGS) -c  $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -o  $(OUT_SIM_PWD)/$(notdir $@)
+	@$(CPPC_SIM) $(CCFLAGS) -MM $< $(DEFINES) $(DEFINES_SIM) $(INCLUDEPATH) -MT $(OUT_SIM_PWD)/$(notdir $@) > $(OUT_SIM_PWD)/$*_simcpp.d
 
-# rule to link SIM_OBJECTS to an elf in OUT_PWD
-$(OUT_PWD)/$(SIM_EXE) : $(SIM_OBJECTS)
-	@printf "$(COMPCOLOR)LD %-36s => %s$(NORM)\n" "*.o" $(OUT_PWD)/$(SIM_EXE)
-	$(VERB)$(CPPC_SIM) $(CCFLAGS) -o $(OUT_PWD)/$(SIM_EXE) $(addprefix $(OUT_PWD)/,$(notdir $(SIM_OBJECTS))) $(LIBS_SIM) $(DEFINES_SIM) -lm
+# rule to link SIM_OBJECTS to an elf in OUT_SIM_PWD
+$(OUT_SIM_PWD)/$(SIM_EXE) : $(SIM_OBJECTS)
+	@printf "$(COMPCOLOR)LD %-36s => %s$(NORM)\n" "*.o" $(OUT_SIM_PWD)/$(SIM_EXE)
+	$(VERB)$(CPPC_SIM) $(CCFLAGS) -o $(OUT_SIM_PWD)/$(SIM_EXE) $(addprefix $(OUT_SIM_PWD)/,$(notdir $(SIM_OBJECTS))) $(LIBS_SIM) $(DEFINES_SIM)
 
-sim-exe: $(OUT_PWD)/$(SIM_EXE)
+sim-exe: $(OUT_SIM_PWD)/$(SIM_EXE)
 
-sim : sim-exe
+sim: sim-exe
 	#killall udk-sim || true
-	$(OUT_PWD)/$(SIM_EXE)
+	$(UDKSIM_EXE) $(OUT_SIM_PWD)/$(SIM_EXE)
 
+clean: sim-clean
 sim-clean :
-	rm -f $(OUT_PWD)/*.o $(OUT_PWD)/*.d $(OUT_PWD)/*.c
-	rm -f $(OUT_PWD)/$(SIM_EXE)
+	rm -f $(OUT_SIM_PWD)*/*.o $(OUT_SIM_PWD)*/*.d $(OUT_SIM_PWD)*/*.c
+	rm -f $(OUT_SIM_PWD)*/$(SIM_EXE)*
