c50a8f136b9383ca637f7fbfe0e4ec736e9bfdff
sebcaux <sebastien.caux@uniswarm.eu>
2021-05-31 22:57:52 +0200

    [timer:dspic33c] Added new devices

--- support/driver/timer/timer_dspic33c.h
+++ support/driver/timer/timer_dspic33c.h
@@ -50,8 +50,12 @@
     || defined(DEVICE_33CK128MP502) || defined(DEVICE_33CK128MP503) || defined(DEVICE_33CK128MP505)                    \
     || defined(DEVICE_33CK128MP506) || defined(DEVICE_33CK128MP508) || defined(DEVICE_33CK256MP202)                    \
     || defined(DEVICE_33CK256MP203) || defined(DEVICE_33CK256MP205) || defined(DEVICE_33CK256MP206)                    \
-    || defined(DEVICE_33CK256MP208) || defined(DEVICE_33CK256MP502) || defined(DEVICE_33CK256MP503)                    \
-    || defined(DEVICE_33CK256MP505) || defined(DEVICE_33CK256MP506) || defined(DEVICE_33CK256MP508)
+    || defined(DEVICE_33CK256MP208) || defined(DEVICE_33CK256MP305) || defined(DEVICE_33CK256MP306)                    \
+    || defined(DEVICE_33CK256MP308) || defined(DEVICE_33CK256MP502) || defined(DEVICE_33CK256MP503)                    \
+    || defined(DEVICE_33CK256MP505) || defined(DEVICE_33CK256MP506) || defined(DEVICE_33CK256MP508)                    \
+    || defined(DEVICE_33CK256MP605) || defined(DEVICE_33CK256MP606) || defined(DEVICE_33CK256MP608)                    \
+    || defined(DEVICE_33CK512MP305) || defined(DEVICE_33CK512MP306) || defined(DEVICE_33CK512MP308)                    \
+    || defined(DEVICE_33CK512MP605) || defined(DEVICE_33CK512MP606) || defined(DEVICE_33CK512MP608)
 #    define TIMER_COUNT 1
 #else
 #    define TIMER_COUNT 0
