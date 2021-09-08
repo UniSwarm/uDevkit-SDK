cc35eebf4d2cb44ae5b2770ee93bff29907bb228
sebcaux <sebastien.caux@uniswarm.eu>
2021-09-08 19:55:51 +0200

    [gpio:pic32] Updated devices list support

--- support/driver/gpio/gpio_pic32.h
+++ support/driver/gpio/gpio_pic32.h
@@ -47,11 +47,10 @@
 #elif defined(DEVICE_32MM0016GPL020) || defined(DEVICE_32MM0032GPL020) || defined(DEVICE_32MM0064GPL020)               \
     || defined(DEVICE_32MX110F016B) || defined(DEVICE_32MX120F032B) || defined(DEVICE_32MX130F064B)                    \
     || defined(DEVICE_32MX130F256B) || defined(DEVICE_32MX150F128B) || defined(DEVICE_32MX154F128B)                    \
-    || defined(DEVICE_32MX155F128B) || defined(DEVICE_32MX170F256B) || defined(DEVICE_32MX174F256B)                    \
-    || defined(DEVICE_32MX175F256B) || defined(DEVICE_32MX210F016B) || defined(DEVICE_32MX220F032B)                    \
-    || defined(DEVICE_32MX230F064B) || defined(DEVICE_32MX230F256B) || defined(DEVICE_32MX250F128B)                    \
-    || defined(DEVICE_32MX254F128B) || defined(DEVICE_32MX255F128B) || defined(DEVICE_32MX270F256B)                    \
-    || defined(DEVICE_32MX274F256B) || defined(DEVICE_32MX275F256B)
+    || defined(DEVICE_32MX170F256B) || defined(DEVICE_32MX174F256B) || defined(DEVICE_32MX210F016B)                    \
+    || defined(DEVICE_32MX220F032B) || defined(DEVICE_32MX230F064B) || defined(DEVICE_32MX230F256B)                    \
+    || defined(DEVICE_32MX250F128B) || defined(DEVICE_32MX254F128B) || defined(DEVICE_32MX270F256B)                    \
+    || defined(DEVICE_32MX274F256B)
 #    define GPIO_HAVE_PORTA
 #    define GPIO_HAVE_PORTB
 #    define GPIO_COUNT 2
@@ -62,31 +61,31 @@
     || defined(DEVICE_32MX110F016C) || defined(DEVICE_32MX110F016D) || defined(DEVICE_32MX120F032C)                    \
     || defined(DEVICE_32MX120F032D) || defined(DEVICE_32MX130F064C) || defined(DEVICE_32MX130F064D)                    \
     || defined(DEVICE_32MX130F256D) || defined(DEVICE_32MX150F128C) || defined(DEVICE_32MX150F128D)                    \
-    || defined(DEVICE_32MX154F128D) || defined(DEVICE_32MX155F128D) || defined(DEVICE_32MX170F256D)                    \
-    || defined(DEVICE_32MX174F256D) || defined(DEVICE_32MX175F256D) || defined(DEVICE_32MX210F016C)                    \
-    || defined(DEVICE_32MX210F016D) || defined(DEVICE_32MX220F032C) || defined(DEVICE_32MX220F032D)                    \
-    || defined(DEVICE_32MX230F064C) || defined(DEVICE_32MX230F064D) || defined(DEVICE_32MX230F256D)                    \
-    || defined(DEVICE_32MX250F128C) || defined(DEVICE_32MX250F128D) || defined(DEVICE_32MX254F128D)                    \
-    || defined(DEVICE_32MX255F128D) || defined(DEVICE_32MX270F256D) || defined(DEVICE_32MX274F256D)                    \
-    || defined(DEVICE_32MX275F256D)
+    || defined(DEVICE_32MX154F128D) || defined(DEVICE_32MX170F256D) || defined(DEVICE_32MX174F256D)                    \
+    || defined(DEVICE_32MX210F016C) || defined(DEVICE_32MX210F016D) || defined(DEVICE_32MX220F032C)                    \
+    || defined(DEVICE_32MX220F032D) || defined(DEVICE_32MX230F064C) || defined(DEVICE_32MX230F064D)                    \
+    || defined(DEVICE_32MX230F256D) || defined(DEVICE_32MX250F128C) || defined(DEVICE_32MX250F128D)                    \
+    || defined(DEVICE_32MX254F128D) || defined(DEVICE_32MX270F256D) || defined(DEVICE_32MX274F256D)
 #    define GPIO_HAVE_PORTA
 #    define GPIO_HAVE_PORTB
 #    define GPIO_HAVE_PORTC
 #    define GPIO_COUNT 3
-#elif defined(DEVICE_32MM0064GPM048) || defined(DEVICE_32MM0064GPM064) || defined(DEVICE_32MM0128GPM048)               \
-    || defined(DEVICE_32MM0128GPM064) || defined(DEVICE_32MM0256GPM048) || defined(DEVICE_32MM0256GPM064)
+#elif defined(DEVICE_32MK0256GPG048) || defined(DEVICE_32MK0256MCJ048) || defined(DEVICE_32MK0512GPG048)               \
+    || defined(DEVICE_32MK0512MCJ048) || defined(DEVICE_32MM0064GPM048) || defined(DEVICE_32MM0064GPM064)              \
+    || defined(DEVICE_32MM0128GPM048) || defined(DEVICE_32MM0128GPM064) || defined(DEVICE_32MM0256GPM048)              \
+    || defined(DEVICE_32MM0256GPM064)
 #    define GPIO_HAVE_PORTA
 #    define GPIO_HAVE_PORTB
 #    define GPIO_HAVE_PORTC
 #    define GPIO_HAVE_PORTD
 #    define GPIO_COUNT 4
-#elif defined(DEVICE_32MK0512GPD064) || defined(DEVICE_32MK0512GPD100) || defined(DEVICE_32MK0512GPE064)               \
-    || defined(DEVICE_32MK0512GPE100) || defined(DEVICE_32MK0512GPK064) || defined(DEVICE_32MK0512GPK100)              \
-    || defined(DEVICE_32MK0512GPL064) || defined(DEVICE_32MK0512GPL100) || defined(DEVICE_32MK0512MCF064)              \
-    || defined(DEVICE_32MK0512MCF100) || defined(DEVICE_32MK0512MCM064) || defined(DEVICE_32MK0512MCM100)              \
-    || defined(DEVICE_32MK1024GPD064) || defined(DEVICE_32MK1024GPD100) || defined(DEVICE_32MK1024GPE064)              \
-    || defined(DEVICE_32MK1024GPE100) || defined(DEVICE_32MK1024GPK064) || defined(DEVICE_32MK1024GPK100)              \
-    || defined(DEVICE_32MK1024GPL064) || defined(DEVICE_32MK1024GPL100) || defined(DEVICE_32MK1024MCF064)              \
+#elif defined(DEVICE_32MK0256GPG064) || defined(DEVICE_32MK0256MCJ064) || defined(DEVICE_32MK0512GPD064)               \
+    || defined(DEVICE_32MK0512GPD100) || defined(DEVICE_32MK0512GPE064) || defined(DEVICE_32MK0512GPE100)              \
+    || defined(DEVICE_32MK0512GPG064) || defined(DEVICE_32MK0512GPK064) || defined(DEVICE_32MK0512GPK100)              \
+    || defined(DEVICE_32MK0512MCF064) || defined(DEVICE_32MK0512MCF100) || defined(DEVICE_32MK0512MCJ064)              \
+    || defined(DEVICE_32MK0512MCM064) || defined(DEVICE_32MK0512MCM100) || defined(DEVICE_32MK1024GPD064)              \
+    || defined(DEVICE_32MK1024GPD100) || defined(DEVICE_32MK1024GPE064) || defined(DEVICE_32MK1024GPE100)              \
+    || defined(DEVICE_32MK1024GPK064) || defined(DEVICE_32MK1024GPK100) || defined(DEVICE_32MK1024MCF064)              \
     || defined(DEVICE_32MK1024MCF100) || defined(DEVICE_32MK1024MCM064) || defined(DEVICE_32MK1024MCM100)              \
     || defined(DEVICE_32MX130F128L) || defined(DEVICE_32MX150F256L) || defined(DEVICE_32MX170F512L)                    \
     || defined(DEVICE_32MX230F128L) || defined(DEVICE_32MX250F256L) || defined(DEVICE_32MX270F512L)                    \
@@ -138,19 +137,29 @@
     || defined(DEVICE_32MZ1025DAA169) || defined(DEVICE_32MZ1025DAA176) || defined(DEVICE_32MZ1025DAA288)              \
     || defined(DEVICE_32MZ1025DAB169) || defined(DEVICE_32MZ1025DAB176) || defined(DEVICE_32MZ1025DAB288)              \
     || defined(DEVICE_32MZ1025DAG169) || defined(DEVICE_32MZ1025DAG176) || defined(DEVICE_32MZ1025DAH169)              \
-    || defined(DEVICE_32MZ1025DAH176) || defined(DEVICE_32MZ1064DAA169) || defined(DEVICE_32MZ1064DAA176)              \
-    || defined(DEVICE_32MZ1064DAA288) || defined(DEVICE_32MZ1064DAB169) || defined(DEVICE_32MZ1064DAB176)              \
-    || defined(DEVICE_32MZ1064DAB288) || defined(DEVICE_32MZ1064DAG169) || defined(DEVICE_32MZ1064DAG176)              \
-    || defined(DEVICE_32MZ1064DAH169) || defined(DEVICE_32MZ1064DAH176) || defined(DEVICE_32MZ2025DAA169)              \
-    || defined(DEVICE_32MZ2025DAA176) || defined(DEVICE_32MZ2025DAA288) || defined(DEVICE_32MZ2025DAB169)              \
-    || defined(DEVICE_32MZ2025DAB176) || defined(DEVICE_32MZ2025DAB288) || defined(DEVICE_32MZ2025DAG169)              \
-    || defined(DEVICE_32MZ2025DAG176) || defined(DEVICE_32MZ2025DAH169) || defined(DEVICE_32MZ2025DAH176)              \
+    || defined(DEVICE_32MZ1025DAH176) || defined(DEVICE_32MZ1025DAK169) || defined(DEVICE_32MZ1025DAK176)              \
+    || defined(DEVICE_32MZ1025DAL169) || defined(DEVICE_32MZ1025DAL176) || defined(DEVICE_32MZ1025DAR169)              \
+    || defined(DEVICE_32MZ1025DAR176) || defined(DEVICE_32MZ1025DAS169) || defined(DEVICE_32MZ1025DAS176)              \
+    || defined(DEVICE_32MZ1064DAA169) || defined(DEVICE_32MZ1064DAA176) || defined(DEVICE_32MZ1064DAA288)              \
+    || defined(DEVICE_32MZ1064DAB169) || defined(DEVICE_32MZ1064DAB176) || defined(DEVICE_32MZ1064DAB288)              \
+    || defined(DEVICE_32MZ1064DAG169) || defined(DEVICE_32MZ1064DAG176) || defined(DEVICE_32MZ1064DAH169)              \
+    || defined(DEVICE_32MZ1064DAH176) || defined(DEVICE_32MZ1064DAK169) || defined(DEVICE_32MZ1064DAK176)              \
+    || defined(DEVICE_32MZ1064DAL169) || defined(DEVICE_32MZ1064DAL176) || defined(DEVICE_32MZ1064DAR169)              \
+    || defined(DEVICE_32MZ1064DAR176) || defined(DEVICE_32MZ1064DAS169) || defined(DEVICE_32MZ1064DAS176)              \
+    || defined(DEVICE_32MZ2025DAA169) || defined(DEVICE_32MZ2025DAA176) || defined(DEVICE_32MZ2025DAA288)              \
+    || defined(DEVICE_32MZ2025DAB169) || defined(DEVICE_32MZ2025DAB176) || defined(DEVICE_32MZ2025DAB288)              \
+    || defined(DEVICE_32MZ2025DAG169) || defined(DEVICE_32MZ2025DAG176) || defined(DEVICE_32MZ2025DAH169)              \
+    || defined(DEVICE_32MZ2025DAH176) || defined(DEVICE_32MZ2025DAK169) || defined(DEVICE_32MZ2025DAK176)              \
+    || defined(DEVICE_32MZ2025DAL169) || defined(DEVICE_32MZ2025DAL176) || defined(DEVICE_32MZ2025DAR169)              \
+    || defined(DEVICE_32MZ2025DAR176) || defined(DEVICE_32MZ2025DAS169) || defined(DEVICE_32MZ2025DAS176)              \
     || defined(DEVICE_32MZ2048ECG144) || defined(DEVICE_32MZ2048ECH144) || defined(DEVICE_32MZ2048ECM144)              \
     || defined(DEVICE_32MZ2048EFG144) || defined(DEVICE_32MZ2048EFH144) || defined(DEVICE_32MZ2048EFM144)              \
     || defined(DEVICE_32MZ2064DAA169) || defined(DEVICE_32MZ2064DAA176) || defined(DEVICE_32MZ2064DAA288)              \
     || defined(DEVICE_32MZ2064DAB169) || defined(DEVICE_32MZ2064DAB176) || defined(DEVICE_32MZ2064DAB288)              \
     || defined(DEVICE_32MZ2064DAG169) || defined(DEVICE_32MZ2064DAG176) || defined(DEVICE_32MZ2064DAH169)              \
-    || defined(DEVICE_32MZ2064DAH176)
+    || defined(DEVICE_32MZ2064DAH176) || defined(DEVICE_32MZ2064DAK169) || defined(DEVICE_32MZ2064DAK176)              \
+    || defined(DEVICE_32MZ2064DAL169) || defined(DEVICE_32MZ2064DAL176) || defined(DEVICE_32MZ2064DAR169)              \
+    || defined(DEVICE_32MZ2064DAR176) || defined(DEVICE_32MZ2064DAS169) || defined(DEVICE_32MZ2064DAS176)
 #    define GPIO_HAVE_PORTA
 #    define GPIO_HAVE_PORTB
 #    define GPIO_HAVE_PORTC
