cb0cddae8ab00dfea52bfeca9e96f0769416ce2e
sebcaux <sebastien.caux@uniswarm.eu>
2022-11-21 22:13:01 +0100

    [can] Added enum helper for CAN_FIFO_ID and CAN_FILTER_ID

--- support/driver/can/can.h
+++ support/driver/can/can.h
@@ -88,6 +88,42 @@ typedef struct
     uint8_t size;
 } CAN_MSG_HEADER;
 
+typedef enum
+{
+    CAN_FIFO00 = 0,
+    CAN_FIFO01 = 1,
+    CAN_FIFO02 = 2,
+    CAN_FIFO03 = 3,
+    CAN_FIFO04 = 4,
+    CAN_FIFO05 = 5,
+    CAN_FIFO06 = 6,
+    CAN_FIFO07 = 7,
+    CAN_FIFO08 = 8,
+    CAN_FIFO09 = 9,
+    CAN_FIFO10 = 10,
+    CAN_FIFO11 = 11,
+    CAN_FIFO12 = 12,
+    CAN_FIFO13 = 13,
+    CAN_FIFO14 = 14,
+    CAN_FIFO15 = 15,
+    CAN_FIFO16 = 16,
+    CAN_FIFO17 = 17,
+    CAN_FIFO18 = 18,
+    CAN_FIFO19 = 19,
+    CAN_FIFO20 = 20,
+    CAN_FIFO21 = 21,
+    CAN_FIFO22 = 22,
+    CAN_FIFO23 = 23,
+    CAN_FIFO24 = 24,
+    CAN_FIFO25 = 25,
+    CAN_FIFO26 = 26,
+    CAN_FIFO27 = 27,
+    CAN_FIFO28 = 28,
+    CAN_FIFO29 = 29,
+    CAN_FIFO30 = 30,
+    CAN_FIFO31 = 31,
+} CAN_FIFO_ID;
+
 int can_send(rt_dev_t device, uint8_t fifo, CAN_MSG_HEADER *header, char *data);
 int can_rec(rt_dev_t device, uint8_t fifo, CAN_MSG_HEADER *header, char *data);
 
@@ -98,6 +134,42 @@ typedef enum
     CAN_FRAME_EXT = 0x02,   ///< CAN extended frames only
     CAN_FRAME_BOTH = 0x03,  ///< CAN standard and extended frames
 } CAN_FRAME_FORMAT_FLAGS;
+
+typedef enum
+{
+    CAN_FILTER00 = 0,
+    CAN_FILTER01 = 1,
+    CAN_FILTER02 = 2,
+    CAN_FILTER03 = 3,
+    CAN_FILTER04 = 4,
+    CAN_FILTER05 = 5,
+    CAN_FILTER06 = 6,
+    CAN_FILTER07 = 7,
+    CAN_FILTER08 = 8,
+    CAN_FILTER09 = 9,
+    CAN_FILTER10 = 10,
+    CAN_FILTER11 = 11,
+    CAN_FILTER12 = 12,
+    CAN_FILTER13 = 13,
+    CAN_FILTER14 = 14,
+    CAN_FILTER15 = 15,
+    CAN_FILTER16 = 16,
+    CAN_FILTER17 = 17,
+    CAN_FILTER18 = 18,
+    CAN_FILTER19 = 19,
+    CAN_FILTER20 = 20,
+    CAN_FILTER21 = 21,
+    CAN_FILTER22 = 22,
+    CAN_FILTER23 = 23,
+    CAN_FILTER24 = 24,
+    CAN_FILTER25 = 25,
+    CAN_FILTER26 = 26,
+    CAN_FILTER27 = 27,
+    CAN_FILTER28 = 28,
+    CAN_FILTER29 = 29,
+    CAN_FILTER30 = 30,
+    CAN_FILTER31 = 31,
+} CAN_FILTER_ID;
 
 int can_filterSet(rt_dev_t device, uint8_t nFilter, uint8_t fifo, uint32_t idFilter, uint32_t mask, CAN_FRAME_FORMAT_FLAGS frame);
 int can_filterEnable(rt_dev_t device, uint8_t nFilter);
