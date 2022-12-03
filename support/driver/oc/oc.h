0f07367e059e9235274bf64adcb4698503bae8e4
sebcaux <sebastien.caux@uniswarm.eu>
2022-12-03 15:02:15 +0100

    [oc] Cleaned header with device specific includes moved to _device.h file

--- support/driver/oc/oc.h
+++ support/driver/oc/oc.h
@@ -16,7 +16,7 @@
 extern "C" {
 #endif
 
-#include <driver/device.h>
+#include "oc_device.h"
 
 // ======== device assignation ========
 #define oc(d) MKDEV(DEV_CLASS_OC, (d)-1)
@@ -46,23 +46,6 @@ uint8_t oc_mode(rt_dev_t device);
 int oc_setRVal(rt_dev_t device, uint32_t rVal, uint32_t rsVal);
 uint32_t oc_rVal(rt_dev_t device);
 uint32_t oc_rsVal(rt_dev_t device);
-
-// ========= specific includes ========
-#if defined(ARCHI_pic24ep) || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev)
-#    include "oc_pic24e_dspic33e.h"
-#elif defined(ARCHI_pic24f) || defined(ARCHI_pic24hj) || defined(ARCHI_dspic30f) || defined(ARCHI_dspic33fj)
-#    include "oc_pic24f_dspic30f_dspic33f.h"
-#elif defined(ARCHI_pic24fj)
-#    if defined(DEVICE_24FJ16MC101) || defined(DEVICE_24FJ16MC102) || defined(DEVICE_24FJ32MC101) || defined(DEVICE_24FJ32MC102) || defined(DEVICE_24FJ32MC104)
-#        include "oc_pic24f_dspic30f_dspic33f.h"
-#    else
-#        include "oc_pic24e_dspic33e.h"
-#    endif
-#elif defined(ARCHI_pic32mk) || defined(ARCHI_pic32mx) || defined(ARCHI_pic32mzec) || defined(ARCHI_pic32mzef) || defined(ARCHI_pic32mzda)
-#    include "oc_pic32.h"
-#else
-#    warning Unsuported ARCHI
-#endif
 
 #ifdef __cplusplus
 }
