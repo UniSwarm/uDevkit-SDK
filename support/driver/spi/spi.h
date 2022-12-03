a0c3326b84e71dbe067f6c343a4ade1186053154
sebcaux <sebastien.caux@uniswarm.eu>
2022-12-03 15:04:44 +0100

    [spi] Cleaned header with device specific includes moved to _device.h file

--- support/driver/spi/spi.h
+++ support/driver/spi/spi.h
@@ -16,7 +16,7 @@
 extern "C" {
 #endif
 
-#include <driver/device.h>
+#include "spi_device.h"
 
 // ====== device assignation ======
 #define spi(d) MKDEV(DEV_CLASS_SPI, (d)-1)
@@ -42,19 +42,6 @@ int spi_flush(rt_dev_t device);
 
 // ========= device read =========
 ssize_t spi_read(rt_dev_t device, char *data, size_t size_max);
-
-// ======= specific include =======
-#if defined(ARCHI_pic24f) || defined(ARCHI_pic24fj) || defined(ARCHI_pic24hj) || defined(ARCHI_dspic30f) || defined(ARCHI_dspic33fj)
-#    include "spi_pic24f_dspic33f.h"
-#elif defined(ARCHI_pic24ep) || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev) || defined(ARCHI_dspic33ck) || defined(ARCHI_dspic33ch)
-#    include "spi_pic24e_dspic33e.h"
-#elif defined(ARCHI_dspic30f)
-#    include "spi_dspic30f.h"
-#elif defined(ARCHI_pic32mm) || defined(ARCHI_pic32mk) || defined(ARCHI_pic32mx) || defined(ARCHI_pic32mzec) || defined(ARCHI_pic32mzef)
-#    include "spi_pic32.h"
-#else
-#    error Unsuported ARCHI
-#endif
 
 #ifdef __cplusplus
 }
---
+++ support/driver/spi/spi_device.h
@@ -0,0 +1,37 @@
+/**
+ * @file spi_device.h
+ * @author Sebastien CAUX (secaux)
+ * @copyright UniSwarm 2022
+ *
+ * @date December 3, 2022, 01:35 PM
+ *
+ * @brief SPI support for specific devices
+ */
+
+#ifndef SPI_DEVICE_H
+#define SPI_DEVICE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <driver/device.h>
+
+// ======= specific include =======
+#if defined(ARCHI_pic24f) || defined(ARCHI_pic24fj) || defined(ARCHI_pic24hj) || defined(ARCHI_dspic30f) || defined(ARCHI_dspic33fj)
+#    include "spi_pic24f_dspic33f.h"
+#elif defined(ARCHI_pic24ep) || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev) || defined(ARCHI_dspic33ck) || defined(ARCHI_dspic33ch)
+#    include "spi_pic24e_dspic33e.h"
+#elif defined(ARCHI_dspic30f)
+#    include "spi_dspic30f.h"
+#elif defined(ARCHI_pic32mm) || defined(ARCHI_pic32mk) || defined(ARCHI_pic32mx) || defined(ARCHI_pic32mzec) || defined(ARCHI_pic32mzef)
+#    include "spi_pic32.h"
+#else
+#    error Unsuported ARCHI
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // SPI_DEVICE_H
