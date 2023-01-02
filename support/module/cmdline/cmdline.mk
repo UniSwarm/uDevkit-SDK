fd90a86214181078ea820b51f9d2044c64bda7ba
sebcaux <sebastien.caux@uniswarm.eu>
2023-01-02 21:10:38 +0100

    [cmdline] Refactored mk file

--- support/module/cmdline/cmdline.mk
+++ support/module/cmdline/cmdline.mk
@@ -7,37 +7,43 @@ vpath %.h $(MODULEPATH) $(MODULEPATH)cmd
 vpath %.c $(MODULEPATH) $(MODULEPATH)cmd
 
 SRC += cmdline.c
-HEADER += cmdline.h
-
-SRC := $(SRC) cmd.c cmd_led.c cmd_reg.c
+HEADER += cmdline.h \
+  cmdline_curses.h \
+  cmd.h \
+  cmds.h \
+  cmd_stdio.h
 
 # include all commands
+SRC += cmd.c \
+  cmd_led.c \
+  cmd_reg.c
+
 ifneq (,$(findstring gpio,$(DRIVERS)))
-  SRC := $(SRC) cmd_gpio.c
+  SRC += cmd_gpio.c
 endif
 ifneq (,$(findstring uart,$(DRIVERS)))
-  SRC := $(SRC) cmd_uart.c
+  SRC += cmd_uart.c
 endif
 ifneq (,$(findstring i2c,$(DRIVERS)))
-  SRC := $(SRC) cmd_i2c.c
+  SRC += cmd_i2c.c
 endif
 ifneq (,$(findstring adc,$(DRIVERS)))
-  SRC := $(SRC) cmd_adc.c
+  SRC += cmd_adc.c
 endif
 ifneq (,$(findstring ax,$(DRIVERS)))
-  SRC := $(SRC) cmd_ax.c
+  SRC += cmd_ax.c
 endif
 ifneq (,$(findstring can,$(DRIVERS)))
-  SRC := $(SRC) cmd_can.c
+  SRC += cmd_can.c
 endif
 ifneq (,$(findstring timer,$(DRIVERS)))
-  SRC := $(SRC) cmd_timer.c
+  SRC += cmd_timer.c
 endif
 
-  SRC := $(SRC) cmd_sysclock.c
+  SRC += cmd_sysclock.c
 
 ifneq (,$(findstring mrobot,$(MODULES)))
-  SRC := $(SRC) cmd_mrobot.c
+  SRC += cmd_mrobot.c
 endif
 
 INCLUDEPATH += -I$(CMDLINE_PATH)
