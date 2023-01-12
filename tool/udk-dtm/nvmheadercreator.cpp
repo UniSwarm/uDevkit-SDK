1238a03d220dcba3184e3894c8e71cb1144e2862
sebcaux <sebastien.caux@uniswarm.eu>
2023-01-12 23:23:19 +0100

    [udk-dtm] Fixed use of program sector for PIC32

--- tool/udk-dtm/nvmheadercreator.cpp
+++ tool/udk-dtm/nvmheadercreator.cpp
@@ -10,6 +10,7 @@ NvmHeaderCreator::NvmHeaderCreator(const EdcDb &db, const QString &picFilter, co
 {
     QMultiMap<QString, QString> programMemBeginMap;
     QMultiMap<QString, QString> programMemEndMap;
+    QMultiMap<QString, QString> programSize;
 
     QRegularExpression picFileRegExp(picFilter);
     for (EDCParser *parser : db.parsers())
@@ -19,9 +20,17 @@ NvmHeaderCreator::NvmHeaderCreator(const EdcDb &db, const QString &picFilter, co
             continue;
         }
 
-        const EDCProgramSpace &flashSpace = parser->programSpaces()["PROGRAM"];
+        double coef = 1;
+        QString flashSector = "CODE";
+        if (parser->programSpaces().constFind(flashSector) == parser->programSpaces().constEnd())
+        {
+            flashSector = "PROGRAM";
+            coef = 3.0 / 2.0;
+        }
+        const EDCProgramSpace &flashSpace = parser->programSpaces()[flashSector];
         programMemBeginMap.insert(hexVal(flashSpace.beginaddr, 6), parser->deviceName());
         programMemEndMap.insert(hexVal(flashSpace.endaddr, 6), parser->deviceName());
+        programSize.insert(QString::number((flashSpace.endaddr - flashSpace.beginaddr) * coef, 'f', 0), parser->deviceName());
     }
 
     *this << "// NVM_PROGRAM_BEGIN"
@@ -32,4 +41,9 @@ NvmHeaderCreator::NvmHeaderCreator(const EdcDb &db, const QString &picFilter, co
     *this << "// NVM_PROGRAM_END"
           << "\n";
     writeSwitchDefineValue(programMemEndMap, "NVM_PROGRAM_END");
+
+    *this << "\n";
+    *this << "// NVM_PROGRAM_SIZE"
+          << "\n";
+    writeSwitchDefineValue(programSize, "NVM_PROGRAM_SIZE");
 }
