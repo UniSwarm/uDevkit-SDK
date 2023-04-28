581da7e8e6653740f550eda8c0a4faf006429473
sebcaux <sebastien.caux@uniswarm.eu>
2023-04-28 20:28:31 +0200

    [board:umc1bd90] Fixed mk mistakes

--- support/board/umc1bd90/umc1bd90.mk
+++ support/board/umc1bd90/umc1bd90.mk
@@ -11,18 +11,10 @@ DRIVERS += gpio ccp
 HEADER += umc1bd90.h
 
 ifdef SECONDARY
- ifeq ($(BOARD_VERSION),$(filter $(BOARD_VERSION),101 102))
-  DEVICE = 33CH128MP506S1
- else
-  DEVICE = 33CH512MP508S1
- endif
+ DEVICE = 33CH512MP508S1
  SRC += umc1bd90_secondary.c
 else
- ifeq ($(BOARD_VERSION),$(filter $(BOARD_VERSION),101 102))
-  DEVICE = 33CH128MP506
- else
-  DEVICE = 33CH512MP508
- endif
+ DEVICE = 33CH512MP508
  SRC += umc1bd90.c
  ARCHI_SRC += umc1bd90_fuses.c
 endif
