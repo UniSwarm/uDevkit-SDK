3a73d7094670d912da57ce145c855694be2bc6c3
sebcaux <sebastien.caux@uniswarm.eu>
2024-08-03 15:33:08 +0200

    [i2c:dspic33a] Added base files for implementation

--- support/driver/i2c/i2c_device.h
+++ support/driver/i2c/i2c_device.h
@@ -21,6 +21,8 @@ extern "C" {
 #if defined(ARCHI_pic24ep) || defined(ARCHI_pic24f) || defined(ARCHI_pic24fj) || defined(ARCHI_pic24hj) || defined(ARCHI_dspic30f) || defined(ARCHI_dspic33fj) \
     || defined(ARCHI_dspic33ep) || defined(ARCHI_dspic33ev) || defined(ARCHI_dspic33ch) || defined(ARCHI_dspic33ck)
 #    include "i2c_pic24_dspic30f_dspic33.h"
+#elif defined(ARCHI_dspic33ak)
+#    include "i2c_dspic33a.h"
 #elif defined(ARCHI_pic32mm) || defined(ARCHI_pic32mk) || defined(ARCHI_pic32mx) || defined(ARCHI_pic32mzec) || defined(ARCHI_pic32mzef)                       \
     || defined(ARCHI_pic32mzda)
 #    include "i2c_pic32.h"
