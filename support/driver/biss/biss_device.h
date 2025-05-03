67a3f2c13604fd2a03848a9269696993dfca031f
sebcaux <sebastien.caux@uniswarm.eu>
2025-05-03 16:24:31 +0200

    [biss:dspic33a_pic32] Added dsPIC33AxxxMC/MP and PIC32AK GC device capabilities

--- support/driver/biss/biss_device.h
+++ support/driver/biss/biss_device.h
@@ -12,15 +12,14 @@
 #define BISS_DEVICE_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include <driver/device.h>
 
 // ======= specific include =======
-#if defined(ARCHI_dspic33ak)
-#    include "biss_dspic33a.h"
+#if defined(ARCHI_dspic33ak) || defined(ARCHI_pic32ak)
+#    include "biss_dspic33a_pic32a.h"
 #else
 #    warning "Unsupported ARCHI"
 #endif
@@ -30,30 +29,6 @@ enum
 {
 #    if BISS_COUNT >= 1
     BISS1_ID = 0,
-#    endif
-#    if BISS_COUNT >= 2
-    BISS2_ID = 1,
-#    endif
-#    if BISS_COUNT >= 3
-    BISS3_ID = 2,
-#    endif
-#    if BISS_COUNT >= 4
-    BISS4_ID = 3,
-#    endif
-#    if BISS_COUNT >= 5
-    BISS5_ID = 4,
-#    endif
-#    if BISS_COUNT >= 6
-    BISS6_ID = 5,
-#    endif
-#    if BISS_COUNT >= 7
-    BISS7_ID = 6,
-#    endif
-#    if BISS_COUNT >= 8
-    BISS8_ID = 7,
-#    endif
-#    if BISS_COUNT >= 9
-    BISS9_ID = 8,
 #    endif
 };
 #endif
