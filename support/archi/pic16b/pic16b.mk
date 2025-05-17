a5cfedfdce7345693ebde24f6cc28cd259edffe6
sebcaux <sebastien.caux@uniswarm.eu>
2025-05-17 22:56:17 +0200

    [arch:pic16b] Added support of XC DSC compiler by default for all dsPIC devices. It can be disable by adding 'XC = XC16' in project Makefile

--- support/archi/pic16b/pic16b.mk
+++ support/archi/pic16b/pic16b.mk
@@ -1,24 +1,40 @@
 
-AS = xc16-as
-CC = xc16-gcc
-LD = xc16-ld
-AR = xc16-ar
-HX = xc16-bin2hex
-SIM = sim30
-OBJDUMP = xc16-objdump
+ifneq (,$(filter dspic%,$(ARCHI)))
+ XC ?= XCDSC
+else
+ XC ?= XC16
+endif
+
+ifeq ($(XC), XC16)
+ XC_PATH = $(abspath $(dir $(lastword $(shell whereis -b xc16-gcc)))..)/
+ AS = $(XC_PATH)bin/xc16-as
+ CC = $(XC_PATH)bin/xc16-gcc
+ LD = $(XC_PATH)bin/xc16-ld
+ AR = $(XC_PATH)bin/xc16-ar
+ HX = $(XC_PATH)bin/xc16-bin2hex
+ SIM = $(XC_PATH)bin/sim30
+ OBJDUMP = xc16-objdump
+else
+ XC_PATH = $(abspath $(dir $(lastword $(shell whereis -b xc-dsc-gcc)))..)/
+ AS = $(XC_PATH)bin/xc-dsc-as
+ CC = $(XC_PATH)bin/xc-dsc-gcc
+ LD = $(XC_PATH)bin/xc-dsc-ld
+ AR = $(XC_PATH)bin/xc-dsc-ar
+ HX = $(XC_PATH)bin/xc-dsc-bin2hex
+ SIM = $(XC_PATH)bin/sim30
+ OBJDUMP = xc-dsc-objdump
+endif
 
 CC_VERSION := $(shell $(CC) --version | egrep -o "v([0-9]+\\.[0-9]+)")
 CC_VERSION_MAJOR := $(shell echo $(CC_VERSION) | cut -f2 -dv | cut -f1 -d.)
 CC_VERSION_MINOR := $(shell echo $(CC_VERSION) | cut -f2 -d.)
 
-#$(info $(CC_VERSION) $(CC_VERSION_MAJOR))
+$(info $(ARCHI) $(XC) $(CC_VERSION) $(CC_VERSION_MAJOR))
 
-XC16_PATH = $(abspath $(dir $(lastword $(shell whereis -b xc16-gcc)))..)/
 ifeq ("$(LK_SCRIPT)","")
  LK_SCRIPT = p$(DEVICE).gld
 endif
 
-ifeq ("$(CC)","xc16-gcc") # XC16 compiler used
  ifeq ("$(CC_VERSION_MAJOR)","1")
   CCFLAGS_XC += -no-legacy-libc
  endif
@@ -28,52 +44,23 @@ ifeq ("$(CC)","xc16-gcc") # XC16 compiler used
  LDFLAGS_XC += -Wl,--heap=$(HEAP),-T$(LK_SCRIPT)
  CCFLAGS += -Wall
 
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic30f))
-  LDFLAGS_XC += -Wl,-L$(XC16_PATH)support/dsPIC30F/gld/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),pic24f pic24fj pic24hj))
-  LDFLAGS_XC += -Wl,-L$(XC16_PATH)support/PIC24F/gld/ -Wl,-L$(XC16_PATH)support/PIC24H/gld/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),pic24ep))
-  LDFLAGS_XC += -Wl,-L$(XC16_PATH)support/PIC24E/gld/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic33fj))
-  LDFLAGS_XC += -Wl,-L$(XC16_PATH)support/dsPIC33F/gld/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ep dspic33ev))
-  LDFLAGS_XC += -Wl,-L$(XC16_PATH)support/dsPIC33E/gld/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ck dspic33ch))
-  LDFLAGS_XC += -Wl,-DLD_PATH=$(XC16_PATH)support/dsPIC33C/gld/
- endif
-
-else                      # other compiler used
- DEFPROC := $(shell echo $(DEVICE) |\
-    sed -e 's/.*\(^[23][42][MEF].*\).*/PIC\1/'\
-    -e 's/.*\(^3[03][EFC][PVJHK]*.*\).*/dsPIC\1/')
- CCFLAGS += -D_HOSTED -D__$(DEFPROC)__ -D__XC16__ -D__prog__ -D__pack_upper_byte 
- CCFLAGS += -Wno-attributes -Wno-unknown-pragmas
- CCFLAGS += -I$(XC16_PATH)include/ -I$(XC16_PATH)support/generic/h/
-
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic30f))
-  CCFLAGS += -D__dsPIC30F__ -I$(XC16_PATH)support/dsPIC30F/h/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),pic24f pic24fj pic24hj))
-  CCFLAGS += -D__PIC24F__ -I$(XC16_PATH)support/PIC24F/h/ -I$(XC16_PATH)support/PIC24H/h/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),pic24ep))
-  CCFLAGS += -D__PIC24E__ -I$(XC16_PATH)support/PIC24E/h/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic33fj))
-  CCFLAGS += -D__dsPIC33F__ -I$(XC16_PATH)support/dsPIC33F/h/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ep dspic33ev))
-  CCFLAGS += -D__dsPIC33E__ -I$(XC16_PATH)support/dsPIC33E/h/
- endif
- ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ck dspic33ch))
-  CCFLAGS += -D__dsPIC33C__ -I$(XC16_PATH)support/dsPIC33C/h/
- endif
- CCFLAGS += -Wno-all
+ifeq ($(ARCHI),$(filter $(ARCHI),dspic30f))
+ LDFLAGS_XC += -Wl,-L$(XC_PATH)support/dsPIC30F/gld/
+endif
+ifeq ($(ARCHI),$(filter $(ARCHI),pic24f pic24fj pic24hj))
+ LDFLAGS_XC += -Wl,-L$(XC_PATH)support/PIC24F/gld/ -Wl,-L$(XC_PATH)support/PIC24H/gld/
+endif
+ifeq ($(ARCHI),$(filter $(ARCHI),pic24ep))
+ LDFLAGS_XC += -Wl,-L$(XC_PATH)support/PIC24E/gld/
+endif
+ifeq ($(ARCHI),$(filter $(ARCHI),dspic33fj))
+ LDFLAGS_XC += -Wl,-L$(XC_PATH)support/dsPIC33F/gld/
+endif
+ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ep dspic33ev))
+ LDFLAGS_XC += -Wl,-L$(XC_PATH)support/dsPIC33E/gld/
+endif
+ifeq ($(ARCHI),$(filter $(ARCHI),dspic33ck dspic33ch))
+ LDFLAGS_XC += -Wl,-DLD_PATH=$(XC_PATH)support/dsPIC33C/gld/
 endif
 
 vpath %.h $(UDEVKIT)/support/archi/pic16b/
