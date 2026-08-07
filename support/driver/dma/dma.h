c3eaeb890d526d99d4d6d6d3761a5b216a5f9aff
sebcaux <sebastien.caux@uniswarm.eu>
2026-08-07 15:02:53 +0200

    [dma] Added a basic API for transfer configuration

--- support/driver/dma/dma.h
+++ support/driver/dma/dma.h
@@ -1,7 +1,7 @@
 /**
  * @file dma.h
  * @author Sebastien CAUX (sebcaux)
- * @copyright UniSwarm 2021-2023
+ * @copyright UniSwarm 2021-2026
  *
  * @date September 11, 2021, 03:50 PM
  *
@@ -17,7 +17,7 @@ extern "C" {
 
 #include "dma_device.h"
 
-void dma_init(uint16_t addrLow, uint16_t addrHigh);
+void dma_init(const void *addrLow, const void *addrHigh);
 
 // ====== device assignation ======
 #define dma(d) MKDEV(DEV_CLASS_DMA, ((d) - 1))
@@ -32,6 +32,17 @@ int dma_disable(rt_dev_t device);
 bool dma_isEnabled(rt_dev_t device);
 
 int dma_setHandler(rt_dev_t device, void (*handler)(void));
+
+// Transfert configuration
+int dma_setSource(rt_dev_t device, const void *srcAddr, uint8_t addrMode);
+int dma_setDestination(rt_dev_t device, void *dstAddr, uint8_t addrMode);
+int dma_setTrigger(rt_dev_t device, uint8_t trigger);
+int dma_setTransferMode(rt_dev_t device, uint8_t transferMode);
+int dma_setTransferCount(rt_dev_t device, uint16_t transferCount);
+int dma_setWordSize(rt_dev_t device, uint8_t wordSize);
+int dma_setOptions(rt_dev_t device, uint8_t options);
+
+int dma_softTrigger(rt_dev_t device);
 
 #ifdef __cplusplus
 }
